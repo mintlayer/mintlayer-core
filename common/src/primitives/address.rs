ca84cae9455bd5f36636afd589ad8df81b771716
b-yap <2826165+b-yap@users.noreply.github.com>
2021-11-30 13:52:20 +0800

    common/address: fix AddressExt

--- common/src/primitives/address.rs
+++ common/src/primitives/address.rs
@@ -1,18 +1,15 @@
-use crate::primitives::{Error, encode,decode, DecodedBech32};
+use crate::primitives::{Error, encode, decode};
 
-pub trait AddressExt {
+pub trait AddressExt<T: AsRef<[u8]>> {
     fn encode_to_address(&self) -> Result<String, Error> {
-        encode(
-            self.get_hrp(),
-                self.get_data()
-        )
+        encode(self.hrp(), self.data())
     }
 
     /// get hrp from the provided Bech32 Address
-    fn get_hrp(s:&str) -> Result<String,Error> {
-        decode(s).map(|d| d.hrp)
+    fn get_hrp(bech32_address:&str) -> Result<String,Error> {
+        decode(bech32_address).map(|d| d.hrp)
     }
 
     fn hrp(&self) -> &str;
-    fn data<T: AsRef<[u8]>>(&self) -> T;
+    fn data(&self) -> T;
 }
