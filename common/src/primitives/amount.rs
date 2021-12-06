9834174fd3de277e19cddc4c4738eeb42a825c6c
Samer Afach <info@afach.de>
2021-12-06 10:42:55 +0100

    Make shifts results optional for safety

--- common/src/primitives/amount.rs
+++ common/src/primitives/amount.rs
@@ -105,35 +105,19 @@ impl std::ops::Not for Amount {
     }
 }
 
-impl std::ops::Shl<usize> for Amount {
-    type Output = Self;
+impl std::ops::Shl<u32> for Amount {
+    type Output = Option<Self>;
 
-    fn shl(self, other: usize) -> Self {
-        Amount {
-            val: self.val.shl(other),
-        }
+    fn shl(self, other: u32) -> Option<Self> {
+        self.val.checked_shl(other).map(|v| { Amount { val: v } })
     }
 }
 
-impl std::ops::ShlAssign<usize> for Amount {
-    fn shl_assign(&mut self, other: usize) {
-        self.val.shl_assign(other)
-    }
-}
+impl std::ops::Shr<u32> for Amount {
+    type Output = Option<Self>;
 
-impl std::ops::Shr<usize> for Amount {
-    type Output = Self;
-
-    fn shr(self, other: usize) -> Self {
-        Amount {
-            val: self.val.shr(other),
-        }
-    }
-}
-
-impl std::ops::ShrAssign<usize> for Amount {
-    fn shr_assign(&mut self, other: usize) {
-        self.val.shr_assign(other)
+    fn shr(self, other: u32) -> Option<Self> {
+        self.val.checked_shr(other).map(|v| { Amount { val: v } })
     }
 }
 
@@ -240,38 +224,15 @@ mod tests {
     #[test]
     fn bit_shifts() {
         let x = Amount { val: 1 };
-        assert_eq!(x << 1, Amount { val: 2 });
-        assert_eq!(x << 2, Amount { val: 4 });
-        assert_eq!(x << 4, Amount { val: 16 });
-        assert_eq!(x << 6, Amount { val: 64 });
+        assert_eq!(x << 1, Some(Amount { val: 2 }));
+        assert_eq!(x << 2, Some(Amount { val: 4 }));
+        assert_eq!(x << 4, Some(Amount { val: 16 }));
+        assert_eq!(x << 6, Some(Amount { val: 64 }));
 
         let y = Amount { val: 128 };
-        assert_eq!(y >> 1, Amount { val: 64 });
-        assert_eq!(y >> 2, Amount { val: 32 });
-        assert_eq!(y >> 4, Amount { val: 8 });
-        assert_eq!(y >> 6, Amount { val: 2 });
-    }
-
-    #[test]
-    fn bit_shifts_assign() {
-        let mut x = Amount { val: 1 };
-        x <<= 1;
-        assert_eq!(x, Amount { val: 2 });
-        x <<= 1;
-        assert_eq!(x, Amount { val: 4 });
-        x <<= 2;
-        assert_eq!(x, Amount { val: 16 });
-        x <<= 2;
-        assert_eq!(x, Amount { val: 64 });
-
-        let mut y = Amount { val: 128 };
-        y >>= 1;
-        assert_eq!(y, Amount { val: 64 });
-        y >>= 1;
-        assert_eq!(y, Amount { val: 32 });
-        y >>= 2;
-        assert_eq!(y, Amount { val: 8 });
-        y >>= 2;
-        assert_eq!(y, Amount { val: 2 });
+        assert_eq!(y >> 1, Some(Amount { val: 64 }));
+        assert_eq!(y >> 2, Some(Amount { val: 32 }));
+        assert_eq!(y >> 4, Some(Amount { val: 8 }));
+        assert_eq!(y >> 6, Some(Amount { val: 2 }));
     }
 }
