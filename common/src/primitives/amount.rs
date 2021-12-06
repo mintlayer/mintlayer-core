0d45383770bf3f11f8fe460eff11c778471bdfb3
Samer Afach <info@afach.de>
2021-12-06 10:00:17 +0100

    Fix tests testing the wrong thing

--- common/src/primitives/amount.rs
+++ common/src/primitives/amount.rs
@@ -105,35 +105,35 @@ impl std::ops::Not for Amount {
     }
 }
 
-impl std::ops::Shl for Amount {
+impl std::ops::Shl<usize> for Amount {
     type Output = Self;
 
-    fn shl(self, other: Self) -> Self {
+    fn shl(self, other: usize) -> Self {
         Amount {
-            val: self.val.shl(other.val),
+            val: self.val.shl(other),
         }
     }
 }
 
-impl std::ops::ShlAssign for Amount {
-    fn shl_assign(&mut self, other: Self) {
-        self.val.shl_assign(other.val)
+impl std::ops::ShlAssign<usize> for Amount {
+    fn shl_assign(&mut self, other: usize) {
+        self.val.shl_assign(other)
     }
 }
 
-impl std::ops::Shr for Amount {
+impl std::ops::Shr<usize> for Amount {
     type Output = Self;
 
-    fn shr(self, other: Self) -> Self {
+    fn shr(self, other: usize) -> Self {
         Amount {
-            val: self.val.shr(other.val),
+            val: self.val.shr(other),
         }
     }
 }
 
-impl std::ops::ShrAssign for Amount {
-    fn shr_assign(&mut self, other: Self) {
-        self.val.shr_assign(other.val)
+impl std::ops::ShrAssign<usize> for Amount {
+    fn shr_assign(&mut self, other: usize) {
+        self.val.shr_assign(other)
     }
 }
 
@@ -213,65 +213,65 @@ mod tests {
 
     #[test]
     fn bit_ops() {
-        let x = 5;
-        let y = 1;
-        let z = 2;
+        let x = Amount { val: 5 };
+        let y = Amount { val: 1 };
+        let z = Amount { val: 2 };
         let zero: IntType = 0;
-        assert_eq!(x | y, 5);
-        assert_eq!(x & z, 0);
-        assert_eq!(x ^ y, 4);
+        assert_eq!(x | y, Amount { val: 5 });
+        assert_eq!(x & z, Amount { val: 0 });
+        assert_eq!(x ^ y, Amount { val: 4 });
         assert!(!zero == IntType::MAX);
     }
 
     #[test]
     fn bit_ops_assign() {
-        let mut x = 5;
+        let mut x = Amount { val: 5 };
 
-        x ^= 1;
-        assert_eq!(x, 4);
+        x ^= Amount { val: 1 };
+        assert_eq!(x, Amount { val: 4 });
 
-        x |= 2;
-        assert_eq!(x, 6);
+        x |= Amount { val: 2 };
+        assert_eq!(x, Amount { val: 6 });
 
-        x &= 5;
-        assert_eq!(x, 4);
+        x &= Amount { val: 5 };
+        assert_eq!(x, Amount { val: 4 });
     }
 
     #[test]
     fn bit_shifts() {
-        let x = 1;
-        assert_eq!(x << 1, 2);
-        assert_eq!(x << 2, 4);
-        assert_eq!(x << 4, 16);
-        assert_eq!(x << 6, 64);
+        let x = Amount { val: 1 };
+        assert_eq!(x << 1, Amount { val: 2 });
+        assert_eq!(x << 2, Amount { val: 4 });
+        assert_eq!(x << 4, Amount { val: 16 });
+        assert_eq!(x << 6, Amount { val: 64 });
 
-        let y = 128;
-        assert_eq!(y >> 1, 64);
-        assert_eq!(y >> 2, 32);
-        assert_eq!(y >> 4, 8);
-        assert_eq!(y >> 6, 2);
+        let y = Amount { val: 128 };
+        assert_eq!(y >> 1, Amount { val: 64 });
+        assert_eq!(y >> 2, Amount { val: 32 });
+        assert_eq!(y >> 4, Amount { val: 8 });
+        assert_eq!(y >> 6, Amount { val: 2 });
     }
 
     #[test]
     fn bit_shifts_assign() {
-        let mut x = 1;
+        let mut x = Amount { val: 1 };
         x <<= 1;
-        assert_eq!(x, 2);
+        assert_eq!(x, Amount { val: 2 });
         x <<= 1;
-        assert_eq!(x, 4);
+        assert_eq!(x, Amount { val: 4 });
         x <<= 2;
-        assert_eq!(x, 16);
+        assert_eq!(x, Amount { val: 16 });
         x <<= 2;
-        assert_eq!(x, 64);
+        assert_eq!(x, Amount { val: 64 });
 
-        let mut y = 128;
+        let mut y = Amount { val: 128 };
         y >>= 1;
-        assert_eq!(y, 64);
+        assert_eq!(y, Amount { val: 64 });
         y >>= 1;
-        assert_eq!(y, 32);
+        assert_eq!(y, Amount { val: 32 });
         y >>= 2;
-        assert_eq!(y, 8);
+        assert_eq!(y, Amount { val: 8 });
         y >>= 2;
-        assert_eq!(y, 2);
+        assert_eq!(y, Amount { val: 2 });
     }
 }
