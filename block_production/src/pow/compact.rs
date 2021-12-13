9d6924d61effaabab384f69d3ec236e1736a2ee6
b-yap <2826165+b-yap@users.noreply.github.com>
2021-12-13 16:46:14 +0800

    feat/block_production: added docs; and Compact implements `From` and `Into` for Uint256

--- block_production/src/pow/compact.rs
+++ block_production/src/pow/compact.rs
@@ -3,12 +3,14 @@ use util::Uint256;
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Debug)]
 pub struct Compact(pub(crate) Vec<u8>);
 
-impl Compact {
-    pub fn convert_to_uint256(&self) -> Uint256 {
+impl From<Uint256> for Compact {
+    fn from(_: Uint256) -> Self {
         todo!()
     }
+}
 
-    pub fn from_uint256(u256: Uint256) -> Self {
+impl Into<Uint256> for Compact {
+    fn into(self) -> Uint256 {
         todo!()
     }
 }
