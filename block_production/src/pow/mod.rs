208bfeb51a551ab2d4b86bed52fa0e5ef732e1cb
b-yap <2826165+b-yap@users.noreply.github.com>
2021-12-13 16:30:28 +0800

    feat/block_production: using rust-bitcoin's uint256 to represent the unsigned 256 bits.
    changed from just `Vec<u8>` to `Compact`

--- block_production/src/pow/mod.rs
+++ block_production/src/pow/mod.rs
@@ -1,21 +1,20 @@
 mod compact;
 mod pow;
-mod u256;
 
 pub use compact::*;
-pub use u256::*;
+pub use pow::Pow;
+
+use util::Uint256;
 
 pub trait ExtractData {
-    fn get_bits(&self) -> Vec<u8>;
+    fn get_bits(&self) -> Compact;
     fn get_nonce(&self) -> u128;
 
-    fn create(bits: &[u8], nonce: u128) -> Self;
+    fn create(bits: &Compact, nonce: u128) -> Self;
 
-    fn get_difficulty(&self) -> U256;
+    fn get_difficulty(&self) -> Uint256;
 }
 
-
-pub trait Hashable {
-    fn hash(&self) -> U256;
+trait Hashable {
+    fn hash(&self) -> Uint256;
 }
-
