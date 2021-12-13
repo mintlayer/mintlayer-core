b753258a7f3241ca632d6a530388c97efce31727
b-yap <2826165+b-yap@users.noreply.github.com>
2021-12-13 17:17:02 +0800

    feat/block_production: added docs from rust bitcoin

--- util/src/internal_macros.rs
+++ util/src/internal_macros.rs
@@ -1,3 +1,17 @@
+// Rust Bitcoin Library
+// Written in 2014 by
+//     Andrew Poelstra <apoelstra@wpsoftware.net>
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to
+// the public domain worldwide. This software is distributed without
+// any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication
+// along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//
+
 /// Implements standard array methods for a given wrapper type
 macro_rules! impl_array_newtype {
     ($thing:ident, $ty:ty, $len:expr) => {
