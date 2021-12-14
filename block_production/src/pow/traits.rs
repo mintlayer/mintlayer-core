45e9be973f32cf073f1486ee80679841d5f4b72a
b-yap <2826165+b-yap@users.noreply.github.com>
2021-12-14 16:58:19 +0800

    feat/block_production:  move util to common: https://github.com/mintlayer/mintlayer-core/pull/44#discussion_r767764495
     segregated the logic of nonce loop, block creation, difficulty checking:
     https://github.com/mintlayer/mintlayer-core/pull/44/files#r767755363
     https://github.com/mintlayer/mintlayer-core/pull/44/files#r767760939
    
     added a `ConsensusParams` where it can be POW, POS, etc:
     https://github.com/mintlayer/mintlayer-core/pull/44/files#r767759231
    
     Compact inner already u32: https://github.com/mintlayer/mintlayer-core/pull/44/files#r767749840

---
+++ block_production/src/pow/traits.rs
@@ -0,0 +1,23 @@
+use crate::pow::Compact;
+use crate::BlockProductionError;
+use common::primitives::Uint256;
+
+pub trait DataExt {
+    /// Returns the bits
+    fn get_bits(&self) -> Compact;
+
+    /// Returns the nonce
+    fn get_nonce(&self) -> u128;
+
+    /// Create a ConsensusData for Proof of Work
+    fn create(bits: &Compact, nonce: u128) -> Self;
+
+    fn empty() -> Self;
+}
+
+/// functions specifically for Proof of Work
+pub trait PowExt {
+    fn calculate_hash(&self) -> Uint256;
+
+    fn mine(&mut self, max_nonce: u128, difficulty: Uint256) -> Result<(), BlockProductionError>;
+}
