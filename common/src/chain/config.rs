53dea08f1aa95d7df261902dfc6f96493a990622
Samer Afach <info@afach.de>
2021-12-20 06:55:48 +0100

    Create mainnet genesis block in ChainConfig

--- common/src/chain/config.rs
+++ common/src/chain/config.rs
@@ -1,3 +1,6 @@
+use crate::chain::block::Block;
+use crate::chain::transaction::Transaction;
+use crate::primitives::id::{Id, H256};
 use crate::primitives::BlockHeight;
 use std::collections::BTreeMap;
 
@@ -24,6 +27,8 @@ pub struct ChainConfig {
     height_checkpoint_data: BTreeMap<BlockHeight, HashType>,
     #[allow(dead_code)]
     magic_bytes: [u8; 4],
+    #[allow(dead_code)]
+    genesis_block: Block,
 }
 
 impl ChainConfig {
@@ -32,14 +37,43 @@ impl ChainConfig {
     }
 }
 
+const MAINNET_ADDRESS_PREFIX: &'static str = "mlt";
+
+fn create_mainnet_genesis() -> Block {
+    use crate::chain::transaction::{Destination, TxInput, TxOutput};
+    use crate::primitives::Amount;
+
+    let genesis_message = b"".to_vec();
+    // let _genesis_mint_receiver = Address::new_with_hrp(MAINNET_ADDRESS_PREFIX, [])
+    //     .expect("Failed to create genesis mint address");
+    let input = TxInput::new(Id::new(&H256::zero()), 0, genesis_message);
+    let output = TxOutput::new(Amount::new(100000000000000), Destination::Address);
+    let tx = Transaction::new(0, vec![input], vec![output], 0)
+        .expect("Failed to create genesis coinbase transaction");
+
+    Block::new(vec![tx], Id::new(&H256::zero()), 1639975460, Vec::new())
+        .expect("Error creating genesis block")
+}
+
 #[allow(dead_code)]
 pub fn create_mainnet() -> ChainConfig {
     ChainConfig {
         chain_type: ChainType::Mainnet,
-        address_prefix: "mlt".to_owned(),
+        address_prefix: MAINNET_ADDRESS_PREFIX.to_owned(),
         height_checkpoint_data: BTreeMap::<BlockHeight, HashType>::new(),
         rpc_port: 15234,
         p2p_port: 8978,
         magic_bytes: [0x1a, 0x64, 0xe5, 0xf1],
+        genesis_block: create_mainnet_genesis(),
+    }
+}
+
+mod tests {
+
+    #[test]
+    #[allow(clippy::eq_op)]
+    fn mainnet_creation() {
+        use super::*;
+        let _config = create_mainnet();
     }
 }
