2e629a3111ae5d010e9fdce31b6c15d1db2917cc
b-yap <2826165+b-yap@users.noreply.github.com>
2022-01-07 15:17:26 +0800

    feat/block_production: rebase on master and fix conflicts

--- block_production/src/pow/mod.rs
+++ block_production/src/pow/mod.rs
@@ -1,25 +1,18 @@
 mod compact;
+pub mod impls;
 mod pow;
-mod u256;
+mod traits;
 
+use crate::{BlockProductionError, ConsensusParams};
 pub use compact::*;
-pub use u256::*;
+pub use pow::Pow;
 
-pub trait ExtractData {
-    /// Returns the bits
-    fn get_bits(&self) -> Compact;
-
-    /// Returns the nonce
-    fn get_nonce(&self) -> u128;
-
-    /// Create a ConsensusData for Proof of Work
-    fn create(bits: &Compact, nonce: u128) -> Self;
-
-    /// the Uint256 conversion of bits
-    fn get_difficulty(&self) -> Uint256;
+pub enum POWError {
+    FailedUInt256ToCompact,
 }
 
-/// Calculates the hash of the block for Proof of Work
-trait Hashable {
-    fn hash(&self) -> Uint256;
+impl Into<BlockProductionError> for POWError {
+    fn into(self) -> BlockProductionError {
+        BlockProductionError::POWError(self)
+    }
 }
