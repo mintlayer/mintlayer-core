fea424d67c258dfc6011dcb49b95682835024fe4
b-yap <2826165+b-yap@users.noreply.github.com>
2022-01-10 17:13:51 +0800

    feat/pow/bitcoin: 2nd iteration: applying the `work_required` and `calculate_net_work_required`

--- block_production/src/lib.rs
+++ block_production/src/lib.rs
@@ -3,7 +3,7 @@ mod pow;
 use crate::pow::{Network as POWNetwork, POWError};
 use common::chain::block::{Block, BlockCreationError, ConsensusData};
 use common::chain::transaction::Transaction;
-use common::primitives::{Uint256, H256};
+use common::primitives::{Id, Uint256, H256};
 
 //TODO: remove until an actual trait is created
 pub trait Chain {
@@ -11,6 +11,7 @@ pub trait Chain {
     fn get_block_number(block_hash: &H256) -> u32;
     fn get_latest_block() -> Block;
     fn get_block_id(block: &Block) -> H256;
+    fn get_block(block_id: &Id<Block>) -> Block;
 
     fn add_block(block: Block);
 }
--- block_production/src/pow/pow.rs
+++ block_production/src/pow/pow.rs
@@ -1,6 +1,10 @@
 use crate::pow::traits::{DataExt, PowExt};
-use crate::pow::Compact;
-use crate::{BlockProducer, BlockProductionError, Chain, ConsensusParams};
+use crate::pow::{
+    actual_timespan, Compact, Network, DIFFICULTY_ADJUSTMENT_INTERVAL, TARGET_SPACING,
+    TARGET_TIMESPAN_SECS,
+};
+use crate::ConsensusParams::POW;
+use crate::{BlockProducer, BlockProductionError, Chain, ConsensusParams, POWNetwork};
 use common::chain::block::{Block, BlockCreationError, ConsensusData};
 use common::chain::transaction::Transaction;
 use common::primitives::{Id, Uint256, H256};
@@ -21,6 +25,10 @@ impl Chain for Pow {
     }
 
     fn get_block_id(block: &Block) -> H256 {
+        todo!()
+    }
+
+    fn get_block(block_id: &Id<Block>) -> Block {
         todo!()
     }
 
@@ -63,14 +71,102 @@ impl Pow {
     pub fn check_difficulty(block: &Block, difficulty: &Uint256) -> bool {
         block.calculate_hash() <= *difficulty
     }
+
     pub fn create_empty_block(
         time: u32,
         transactions: Vec<Transaction>,
     ) -> Result<Block, BlockCreationError> {
-        let x: Block = Self::get_latest_block();
-
         let hash_prev_block = Self::get_latest_block().get_merkle_root();
         let hash_prev_block = Id::new(&hash_prev_block);
         Block::new(transactions, hash_prev_block, time, ConsensusData::empty())
+    }
+
+    pub fn is_retarget_needed(block: &Block) -> bool {
+        let hash = block.get_merkle_root();
+        let block_height = Self::get_block_number(&hash);
+
+        block_height % DIFFICULTY_ADJUSTMENT_INTERVAL != 0
+    }
+
+    fn last_non_special_min_difficulty(block: &Block, network_limit: Compact) -> Compact {
+        let mut block = block.clone();
+        // Return the last non-special-min-difficulty-rules-block
+        loop {
+            let height = Self::get_block_number(&block.get_merkle_root());
+            let block_bits = block.get_consensus_data().get_bits();
+            if height == 0 {
+                return block_bits;
+            }
+
+            if Self::is_retarget_needed(&block) && block_bits == network_limit {
+                let prev_block_id = block.get_prev_block_id();
+                block = Self::get_block(&prev_block_id);
+            }
+        }
+    }
+
+    fn calculate_next_work_required(
+        prev_block: &Block,
+        curr_block: &Block,
+        network: &Network,
+    ) -> Option<Compact> {
+        let network_limit = network.limit();
+        if network.no_retargeting() {
+            return Some(prev_block.get_consensus_data().get_bits());
+        }
+
+        // limit adjustment step
+        let actual_timespan =
+            actual_timespan(curr_block.get_block_time(), prev_block.get_block_time());
+
+        prev_block
+            .get_consensus_data()
+            .get_bits()
+            .into_uint256()
+            .map(|bits| {
+                let bits = bits.mul_u32(actual_timespan);
+                let bits = bits
+                    / Uint256::from_u64(TARGET_TIMESPAN_SECS as u64).expect("this should not fail");
+
+                if bits > network_limit {
+                    network_limit
+                } else {
+                    bits
+                }
+            })
+            .and_then(|result| Compact::from_uint256(result))
+    }
+
+    pub fn check_for_work_required(new_block: &Block, network: &POWNetwork) -> Option<Compact> {
+        let network_limit = Compact::from_uint256(network.limit());
+
+        let prev_block_id = new_block.get_prev_block_id();
+        let mut prev_block = Self::get_block(&prev_block_id);
+
+        if Self::get_block_number(&prev_block.get_merkle_root()) == 0 {
+            return network_limit;
+        }
+
+        if Self::is_retarget_needed(&new_block) {
+            if network.allow_min_difficulty_blocks() {
+                // Special difficulty rule for testnet:
+                // If the new block's timestamp is more than 2* 10 minutes
+                // then allow mining of a min-difficulty block.
+                return if new_block.get_block_time()
+                    > (prev_block.get_block_time() + (TARGET_SPACING * 2))
+                {
+                    network_limit
+                } else {
+                    // Return the last non-special-min-difficulty-rules-block
+                    network_limit.map(|net_limit| {
+                        Self::last_non_special_min_difficulty(&prev_block, net_limit)
+                    })
+                };
+            }
+
+            return Some(prev_block.get_consensus_data().get_bits());
+        }
+
+        Self::calculate_next_work_required(&prev_block, new_block, network)
     }
 }
