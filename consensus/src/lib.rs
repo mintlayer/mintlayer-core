518f7fcb452e1bca53b29e22c02bfe2c6756ab37
Anton Sinitsyn <antony@email.su>
2022-01-11 11:01:55 +0300

    Updated process_block function

--- consensus/src/lib.rs
+++ consensus/src/lib.rs
@@ -1,23 +1,45 @@
-// use blockchain_storage::BlockchainStorage;
-// use common::chain::config::ChainConfig;
-// use common::chain::block::Block;
+use blockchain_storage::{BlockchainStorage, BlockchainStorageError};
+use common::chain::block::Block;
+use common::chain::config::ChainConfig;
 
 mod chain_state;
-
 mod orphan_blocks;
-// use orphan_blocks::OrphanBlocks;
+use orphan_blocks::OrphanBlocksPool;
 
-// struct Consensus<S: BlockchainStorage> {
-//     chain_config: ChainConfig,
-//     blockchain_storage: S,
-//     orphan_blocks: OrphanBlocks,
-// }
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum BlockStatus {
+    Valid,
+    Failed,
+    // To be expanded
+}
 
-// impl<S: BlockchainStorage> Consensus<S> {
-//     pub fn process_block(block: Block) {
+struct Consensus<S: BlockchainStorage> {
+    chain_config: ChainConfig,
+    blockchain_storage: S,
+    orphan_blocks: OrphanBlocksPool,
+}
 
-//     }
-// }
+impl<S: BlockchainStorage> Consensus<S> {
+    pub fn process_block(&self, block: &Block) -> BlockStatus {
+        match self.check_block(block) {
+            BlockStatus::Valid => {
+                match <S as BlockchainStorage>::set_block(self.blockchain_storage.clone(), block)
+                    .is_ok()
+                {
+                    true => BlockStatus::Valid,
+                    false => BlockStatus::Failed,
+                }
+            }
+            BlockStatus::Failed => BlockStatus::Failed,
+        }
+    }
+
+    fn check_block(&self, _block: &Block) -> BlockStatus {
+        // Will have added some checks
+        BlockStatus::Valid
+    }
+}
 
 #[cfg(test)]
 mod tests {
