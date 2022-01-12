4f18fbfc967648bb85ef9d236789141a45009f21
b-yap <2826165+b-yap@users.noreply.github.com>
2022-01-12 14:33:07 +0800

    feat/pow/bitcoin: fix warnings, and cargo fmt

--- block_production/src/pow/mod.rs
+++ block_production/src/pow/mod.rs
@@ -1,38 +1,14 @@
 mod compact;
+mod constants;
 pub mod impls;
 mod network;
 mod pow;
 mod traits;
 
-use crate::{BlockProductionError, ConsensusParams};
+use crate::BlockProductionError;
 pub use compact::*;
 pub use network::Network;
 pub use pow::Pow;
-
-pub const TARGET_TIMESPAN_SECS: u32 = 14 * 24 * 60 * 60; // 2 weeks
-
-pub const TARGET_SPACING: u32 = 10 * 60;
-pub const DIFFICULTY_ADJUSTMENT_INTERVAL: u32 = TARGET_TIMESPAN_SECS / TARGET_SPACING;
-
-/// taken from: https://github.com/bitcoin/bitcoin/blob/master/src/pow.cpp#L56
-pub const TIMESPAN_ADJUSTMENT_FACTOR: u32 = 4;
-
-pub const UPPER_TARGET_TIMESPAN_SECS: u32 = TARGET_TIMESPAN_SECS * TIMESPAN_ADJUSTMENT_FACTOR;
-pub const LOWER_TARGET_TIMESPAN_SECS: u32 = TARGET_TIMESPAN_SECS / TIMESPAN_ADJUSTMENT_FACTOR;
-
-pub(crate) fn actual_timespan(curr_block_blocktime: u32, prev_block_blocktime: u32) -> u32 {
-    let mut actual_timespan = prev_block_blocktime - curr_block_blocktime;
-
-    if actual_timespan < LOWER_TARGET_TIMESPAN_SECS {
-        actual_timespan = LOWER_TARGET_TIMESPAN_SECS;
-    }
-
-    if actual_timespan > UPPER_TARGET_TIMESPAN_SECS {
-        actual_timespan = UPPER_TARGET_TIMESPAN_SECS;
-    }
-
-    actual_timespan
-}
 
 pub enum POWError {
     FailedUInt256ToCompact,
