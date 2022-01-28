88705a62b0bec6b49faccdc5c71d3c930349130f
Aaro Altonen <aaro.altonen@mintlayer.org>
2022-01-28 13:23:56 +0200

    p2p: Separate P2P tests into unit and integration tests
    Test individual functions in src/lib.rs and move all more complicated
    tests into tests/p2p.rs.

--- p2p/test-utils/src/lib.rs
+++ p2p/test-utils/src/lib.rs
@@ -14,6 +14,36 @@
 // limitations under the License.
 //
 // Author(s): A. Altonen
+#![allow(clippy::mutex_atomic)]
+use lazy_static::lazy_static;
+use std::net::TcpListener;
+use std::sync::Mutex;
+
+/// TCP server address for testing mock service connectivity
+pub const TCP_SERVER: &str = "[::1]:7999";
+
+lazy_static! {
+    static ref TCP_RUNNING: Mutex<bool> = Default::default();
+}
+
+/// Spawn a TCP server if it's not already running
+///
+/// The server is not used for any communication but
+/// only for creating a connected socket for P2P/Peer
+pub fn start_tcp_server() {
+    let mut running = TCP_RUNNING.lock().unwrap();
+    if !(*running) {
+        let server = TcpListener::bind(TCP_SERVER).unwrap();
+
+        std::thread::spawn(move || loop {
+            let (_, _) = server.accept().unwrap();
+        });
+
+        *running = true;
+    }
+}
+
+/// Allocate a port and create a socket address for given NetworkService
 pub fn make_address<T>(addr: &str) -> T
 where
     T: std::str::FromStr,
