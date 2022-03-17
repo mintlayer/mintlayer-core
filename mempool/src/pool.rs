772a4e7ae46b5b89c7f8894b2c5c8353f6eee2a8
Roy Ben Abraham <roy.benabraham@rbblab.com>
2022-03-17 16:49:27 +0700

    Add test for different_size_txs

--- mempool/src/pool.rs
+++ mempool/src/pool.rs
@@ -2366,4 +2366,37 @@ mod tests {
         );
         Ok(())
     }
+
+    #[test]
+    fn different_size_txs() -> anyhow::Result<()> {
+        use std::time::Duration;
+        use std::time::Instant;
+        let mut mempool = setup();
+        let initial_tx = TxGenerator::new()
+            .with_num_inputs(1)
+            .with_num_outputs(10_000)
+            .generate_tx(&mempool)?;
+        mempool.add_transaction(initial_tx)?;
+
+        let target_txs = 100;
+        let mut time_processing_txs = Duration::from_millis(0);
+        let mut time_creating_txs = Duration::from_millis(0);
+        for i in 0..target_txs {
+            let num_inputs = i + 1;
+            let num_outputs = i + 1;
+            let before_creating = Instant::now();
+            let tx = TxGenerator::new()
+                .with_num_inputs(num_inputs)
+                .with_num_outputs(num_outputs)
+                .generate_tx(&mempool)?;
+            time_creating_txs += before_creating.elapsed();
+            let before_processing = Instant::now();
+            mempool.add_transaction(tx)?;
+            time_processing_txs += before_processing.elapsed()
+        }
+
+        println!("Total time spent processing: {:?}", time_processing_txs);
+        println!("Total time spent creating: {:?}", time_creating_txs);
+        Ok(())
+    }
 }
