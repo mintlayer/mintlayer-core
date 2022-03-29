c699f11ebe07ae7ceee50ea867fecf429138eb84
Samer Afach <info@afach.de>
2022-03-29 13:44:55 +0200

    Add BlockDistance and safe arithmetic operations for height

--- common/src/primitives/height.rs
+++ common/src/primitives/height.rs
@@ -1,9 +1,12 @@
 use parity_scale_codec_derive::{Decode, Encode};
 use std::fmt;
-use std::ops::Add;
+use std::ops::{Add, Sub};
+
+type HeightIntType = u64;
+type DistanceIntType = i64;
 
 #[derive(Debug, Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Encode, Decode)]
-pub struct BlockHeight(u64);
+pub struct BlockHeight(HeightIntType);
 
 // Display should be defined for thiserr crate
 impl fmt::Display for BlockHeight {
@@ -12,35 +15,68 @@ impl fmt::Display for BlockHeight {
     }
 }
 
-impl From<BlockHeight> for u64 {
-    fn from(block_height: BlockHeight) -> u64 {
+impl From<BlockHeight> for HeightIntType {
+    fn from(block_height: BlockHeight) -> HeightIntType {
         block_height.inner()
     }
 }
 
-impl From<u64> for BlockHeight {
-    fn from(w: u64) -> BlockHeight {
+impl From<HeightIntType> for BlockHeight {
+    fn from(w: HeightIntType) -> BlockHeight {
         BlockHeight(w)
     }
 }
 
-impl Add<u64> for BlockHeight {
-    type Output = Self;
+impl Add<BlockDistance> for BlockHeight {
+    type Output = Option<Self>;
 
-    fn add(self, other: u64) -> Self {
-        Self(self.0.checked_add(other).expect("overflow when adding BlockHeight to instant"))
+    fn add(self, other: BlockDistance) -> Option<Self> {
+        let height: i64 = self.0 as i64;
+        if height < 0 {
+            return None;
+        }
+
+        let result = height
+            .checked_add(other.0)
+            .expect("overflow when adding BlockHeight to instant");
+        if result < 0 {
+            return None;
+        }
+
+        let result = result as u64;
+        Some(Self(result))
+    }
+}
+
+impl Add<BlockDistance> for BlockDistance {
+    type Output = Option<BlockDistance>;
+
+    fn add(self, other: BlockDistance) -> Option<BlockDistance> {
+        Some(BlockDistance(self.0.checked_add(other.0)?))
+    }
+}
+
+impl Sub<BlockHeight> for BlockHeight {
+    type Output = Option<BlockDistance>;
+
+    fn sub(self, other: BlockHeight) -> Option<BlockDistance> {
+        let h1 = self.0 as i64;
+        let h2 = other.0 as i64;
+
+        if h1 < 0 || h2 < 0 {
+            return None;
+        }
+
+        Some(BlockDistance(h1 - h2))
     }
 }
 
 const ZERO: BlockHeight = BlockHeight(0);
 const ONE: BlockHeight = BlockHeight(1);
-const MAX: BlockHeight = BlockHeight(u64::MAX);
-
-// TODO: for discussion, comment from Lukas:
-// https://github.com/mintlayer/mintlayer-core/pull/70#discussion_r793762390
+const MAX: BlockHeight = BlockHeight(HeightIntType::MAX);
 
 impl BlockHeight {
-    pub fn new(height: u64) -> Self {
+    pub fn new(height: HeightIntType) -> Self {
         Self(height)
     }
 
@@ -56,15 +92,39 @@ impl BlockHeight {
         MAX
     }
 
-    pub fn inner(self) -> u64 {
+    pub fn inner(self) -> HeightIntType {
         self.0
     }
 
-    pub fn checked_add(&self, rhs: u64) -> Option<Self> {
+    pub fn checked_add(&self, rhs: HeightIntType) -> Option<Self> {
         self.0.checked_add(rhs).map(Self::new)
     }
 
     pub fn next_height(&self) -> BlockHeight {
         BlockHeight(self.0 + 1)
+    }
+}
+
+/////////////////////////////
+
+#[derive(Debug, Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Encode, Decode)]
+pub struct BlockDistance(DistanceIntType);
+
+// Display should be defined for thiserr crate
+impl fmt::Display for BlockDistance {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl From<BlockDistance> for DistanceIntType {
+    fn from(block_height: BlockDistance) -> DistanceIntType {
+        block_height.0
+    }
+}
+
+impl From<DistanceIntType> for BlockDistance {
+    fn from(w: DistanceIntType) -> BlockDistance {
+        BlockDistance(w)
     }
 }
