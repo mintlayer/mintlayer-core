9aba26550222421c9b66cd7c225be7602e850bfc
Samer Afach <info@afach.de>
2022-04-23 15:05:11 +0200

    Figured out a way to replace enum values without cloning

--- consensus/src/spend_cache.rs
+++ consensus/src/spend_cache.rs
@@ -19,39 +19,45 @@ enum CachedInputsOperation {
 
 impl CachedInputsOperation {
     fn spend(&mut self, output_index: u32, spender: Spender) -> Result<(), BlockError> {
-        let result = match self {
-            CachedInputsOperation::Write(tx_index) => CachedInputsOperation::Write({
-                tx_index.spend(output_index, spender).map_err(BlockError::from)?;
-                tx_index.clone() // TODO: improve by moving out of the enum and consuming it
-            }),
-            CachedInputsOperation::Read(tx_index) => CachedInputsOperation::Write({
-                tx_index.spend(output_index, spender).map_err(BlockError::from)?;
-                tx_index.clone() // TODO: improve by moving out of the enum and consuming it
-            }),
+        // spend the output
+        match self {
+            CachedInputsOperation::Write(tx_index) | CachedInputsOperation::Read(tx_index) => {
+                tx_index.spend(output_index, spender).map_err(BlockError::from)?
+            }
             CachedInputsOperation::Erase => {
                 return Err(BlockError::MissingOutputOrSpentOutputErased)
             }
-        };
-        let _ = std::mem::replace(self, result);
+        }
+
+        self.mark_as_write();
+
         Ok(())
     }
 
     fn unspend(&mut self, output_index: u32) -> Result<(), BlockError> {
-        let result = match self {
-            CachedInputsOperation::Write(tx_index) => CachedInputsOperation::Write({
-                tx_index.unspend(output_index).map_err(BlockError::from)?;
-                tx_index.clone() // TODO: improve by moving out of the enum and consuming it
-            }),
-            CachedInputsOperation::Read(tx_index) => CachedInputsOperation::Write({
-                tx_index.unspend(output_index).map_err(BlockError::from)?;
-                tx_index.clone() // TODO: improve by moving out of the enum and consuming it
-            }),
+        // spend the output
+        match self {
+            CachedInputsOperation::Write(tx_index) | CachedInputsOperation::Read(tx_index) => {
+                tx_index.unspend(output_index).map_err(BlockError::from)?
+            }
             CachedInputsOperation::Erase => {
                 return Err(BlockError::MissingOutputOrSpentOutputErased)
             }
-        };
-        let _ = std::mem::replace(self, result);
+        }
+
+        self.mark_as_write();
+
         Ok(())
+    }
+
+    fn mark_as_write(&mut self) {
+        // replace &mut self with a new value (must be done like this because it's unsafe)
+        let replacer_func = |self_| match self_ {
+            CachedInputsOperation::Write(tx_index) => CachedInputsOperation::Write(tx_index),
+            CachedInputsOperation::Read(tx_index) => CachedInputsOperation::Write(tx_index),
+            CachedInputsOperation::Erase => unreachable!(),
+        };
+        replace_with::replace_with_or_abort(self, replacer_func);
     }
 }
 
