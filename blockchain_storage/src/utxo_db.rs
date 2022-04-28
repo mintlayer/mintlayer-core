17a5a58ea3bc0fd09c96f1ec3facc09ba7e5f0c9
Samer Afach <info@afach.de>
2022-04-28 10:11:43 +0200

    Remove already fixed todo

--- blockchain_storage/src/utxo_db.rs
+++ blockchain_storage/src/utxo_db.rs
@@ -28,11 +28,9 @@ impl UtxosPersistentStorage for UtxoDBInterface {
         self.store.get_utxo(outpoint).map_err(|e| e.into())
     }
     fn set_best_block_id(&mut self, block_id: &Id<Block>) -> Result<(), utxo::Error> {
-        // TODO: fix; don't store in general block id
         self.store.set_best_block_for_utxos(block_id).map_err(|e| e.into())
     }
     fn get_best_block_id(&self) -> Result<Option<Id<Block>>, utxo::Error> {
-        // TODO: fix; don't get general block id
         self.store.get_best_block_for_utxos().map_err(|e| e.into())
     }
 
