2afc242fefd1305af5c8cd8939cd9483d18fecd2
Aaro Altonen <aaro.altonen@mintlayer.org>
2022-04-29 11:27:14 +0300

    net/libp2p: Add tests for libp2p::ping

--- p2p/src/net/libp2p/proto/ping.rs
+++ p2p/src/net/libp2p/proto/ping.rs
@@ -16,7 +16,7 @@
 // Author(s): A. Altonen
 use crate::{
     error::{self, P2pError},
-    net::libp2p::backend::Backend,
+    net::libp2p::{backend::Backend, types},
 };
 use libp2p::ping::{self, PingEvent};
 use logging::log;
@@ -28,39 +28,41 @@ impl Backend {
                 peer,
                 result: Result::Ok(ping::Success::Ping { rtt }),
             } => {
-                // println!(
-                //     "ping: rtt to {} is {} ms",
-                //     peer.to_base58(),
-                //     rtt.as_millis()
-                // );
+                // TODO: report rtt to swarm manager?
+                log::debug!("peer {:?} responded to ping, rtt {:?}", peer, rtt);
                 Ok(())
             }
             ping::Event {
                 peer,
                 result: Result::Ok(ping::Success::Pong),
             } => {
-                // println!("ping: pong from {}", peer.to_base58());
+                log::debug!("peer {:?} responded to pong", peer);
                 Ok(())
             }
             ping::Event {
                 peer,
                 result: Result::Err(ping::Failure::Timeout),
             } => {
-                // println!("ping: timeout to {}", peer.to_base58());
+                log::warn!("ping timeout for peer {:?}", peer);
                 Ok(())
             }
             ping::Event {
                 peer,
                 result: Result::Err(ping::Failure::Unsupported),
             } => {
-                // println!("ping: {} does not support ping protocol", peer.to_base58());
-                Ok(())
+                log::error!("peer {:?} doesn't support libp2p::ping", peer);
+
+                self.swarm.disconnect_peer_id(peer);
+                self.conn_tx
+                    .send(types::ConnectivityEvent::PeerDisconnected { peer_id: peer })
+                    .await
+                    .map_err(P2pError::from)
             }
             ping::Event {
                 peer,
                 result: Result::Err(ping::Failure::Other { error }),
             } => {
-                // println!("ping: ping::Failure with {}: {}", peer.to_base58(), error);
+                log::error!("unknown ping failure: {:?}", error);
                 Ok(())
             }
         }
@@ -70,7 +72,180 @@ impl Backend {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::net::libp2p::{proto::util, types};
+    use futures::StreamExt;
+    use libp2p::{
+        ping,
+        swarm::{SwarmBuilder, SwarmEvent},
+        Multiaddr,
+    };
+    use std::time::Duration;
 
     #[tokio::test]
-    async fn it_works() {}
+    async fn test_successful_ping_pong() {
+        let addr: Multiaddr = test_utils::make_address("/ip6/::1/tcp/");
+        let (mut backend1, _, mut conn_rx, mut gossip_rx, _) =
+            util::make_libp2p(common::chain::config::create_mainnet(), addr.clone(), &[]).await;
+
+        let addr2: Multiaddr = test_utils::make_address("/ip6/::1/tcp/");
+        let (mut backend2, _, _, _, _) =
+            util::make_libp2p(common::chain::config::create_mainnet(), addr2.clone(), &[]).await;
+
+        util::connect_swarms::<types::ComposedBehaviour, types::ComposedBehaviour>(
+            addr,
+            &mut backend1.swarm,
+            &mut backend2.swarm,
+        )
+        .await;
+
+        let mut needed_events: i32 = 2;
+
+        loop {
+            tokio::select! {
+                event = backend1.swarm.next() => match event {
+                    Some(SwarmEvent::Behaviour(types::ComposedEvent::PingEvent(
+                        ping::Event { result: Result::Ok(_), .. }
+                    ))) => {
+                        needed_events -= 1;
+                    }
+                    Some(_) => {}
+                    None => panic!("channel closed"),
+                },
+                event = backend2.swarm.next() => match event {
+                    Some(SwarmEvent::Behaviour(types::ComposedEvent::PingEvent(
+                        ping::Event { result: Result::Ok(_), .. }
+                    ))) => {
+                        needed_events -= 1;
+                    }
+                    Some(_) => {}
+                    None => panic!("channel closed"),
+                },
+            }
+
+            if needed_events == 0 {
+                break;
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_remote_doesnt_respond() {
+        // TODO: add better test utilites
+        let addr: Multiaddr = test_utils::make_address("/ip6/::1/tcp/");
+        let (mut backend1, _, mut conn_rx, mut gossip_rx, _) = util::make_libp2p_with_ping(
+            common::chain::config::create_mainnet(),
+            addr.clone(),
+            &[],
+            util::make_ping(
+                Some(Duration::from_secs(2)),
+                Some(Duration::from_secs(2)),
+                Some(3),
+            ),
+        )
+        .await;
+
+        let (transport, peer_id, id_keys) = util::make_transport_and_keys();
+        let mut swarm = SwarmBuilder::new(
+            transport,
+            util::make_ping(
+                Some(Duration::from_secs(2)),
+                Some(Duration::from_secs(2)),
+                Some(3),
+            ),
+            peer_id,
+        )
+        .build();
+
+        util::connect_swarms::<types::ComposedBehaviour, ping::Behaviour>(
+            addr,
+            &mut backend1.swarm,
+            &mut swarm,
+        )
+        .await;
+        let mut needed_events: i32 = 2;
+
+        loop {
+            tokio::select! {
+                event = backend1.swarm.next() => match event {
+                    Some(SwarmEvent::Behaviour(types::ComposedEvent::PingEvent(
+                        ping::Event { result: Result::Err(e), .. }
+                    ))) => {
+                        if let ping::Failure::Timeout = e {
+                            needed_events -= 1;
+                        } else {
+                            panic!("invalid ping event received");
+                        }
+                    }
+                    Some(_) => {}
+                    None => panic!("channel closed"),
+                },
+            }
+
+            if needed_events == 0 {
+                break;
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_ping_not_supported() {
+        let addr: Multiaddr = test_utils::make_address("/ip6/::1/tcp/");
+        let config = common::chain::config::create_mainnet();
+        let (mut backend1, _, mut conn_rx, mut gossip_rx, _) = util::make_libp2p_with_ping(
+            config.clone(),
+            addr.clone(),
+            &[],
+            util::make_ping(
+                Some(Duration::from_secs(2)),
+                Some(Duration::from_secs(2)),
+                Some(3),
+            ),
+        )
+        .await;
+
+        let (transport, peer_id, id_keys) = util::make_transport_and_keys();
+        let mut swarm =
+            SwarmBuilder::new(transport, util::make_identify(config, id_keys), peer_id).build();
+
+        util::connect_swarms::<types::ComposedBehaviour, libp2p::identify::Identify>(
+            addr,
+            &mut backend1.swarm,
+            &mut swarm,
+        )
+        .await;
+
+        let mut needed_events: i32 = 2;
+        loop {
+            tokio::select! {
+                event = backend1.swarm.next() => match event {
+                    Some(SwarmEvent::Behaviour(types::ComposedEvent::PingEvent(inner))) => {
+                        if let ping::Event { result: Result::Err(ping::Failure::Unsupported), peer } = inner {
+                            assert_eq!(
+                                backend1.on_ping_event(inner).await,
+                                Ok(())
+                            );
+                            assert_eq!(
+                                conn_rx.try_recv(),
+                                Ok(types::ConnectivityEvent::PeerDisconnected { peer_id: peer })
+                            );
+                            needed_events -= 1;
+                        }
+                    }
+                    Some(SwarmEvent::ConnectionClosed { .. }) => {
+                        needed_events -= 1;
+                    }
+                    Some(_) => {}
+                    None => panic!("channel closed"),
+                },
+                event = swarm.next() => match event {
+                    Some(_) => {}
+                    None => panic!("channel closed"),
+                }
+            }
+
+            if needed_events == 0 {
+                break;
+            }
+        }
+    }
 }
