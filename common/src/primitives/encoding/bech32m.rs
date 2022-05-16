1b102aa87f11f67e3e0d8cd08ae4e6290d8f1408
Samer Afach <info@afach.de>
2022-05-16 08:51:07 +0200

    Use AsRef in a few places instead of slices

--- common/src/primitives/encoding/bech32m.rs
+++ common/src/primitives/encoding/bech32m.rs
@@ -4,8 +4,11 @@ use super::DecodedBase32FromBech32;
 use bech32::u5;
 use bech32::{self, Variant};
 
-pub fn base32_to_bech32m<T: AsRef<[u5]>>(hrp: &str, data: T) -> Result<String, Bech32Error> {
-    bech32::encode(hrp, data, Variant::Bech32m).map_err(|e| e.into())
+pub fn base32_to_bech32m<S: AsRef<str>, T: AsRef<[u5]>>(
+    hrp: S,
+    data: T,
+) -> Result<String, Bech32Error> {
+    bech32::encode(hrp.as_ref(), data, Variant::Bech32m).map_err(|e| e.into())
 }
 
 #[allow(dead_code)]
@@ -28,16 +31,18 @@ pub fn bech32m_to_base32(s: &str) -> Result<DecodedBase32FromBech32, Bech32Error
     }
 }
 
-pub fn arbitrary_data_to_bech32m<T: AsRef<[u8]>>(
-    hrp: &str,
+pub fn arbitrary_data_to_bech32m<S: AsRef<str>, T: AsRef<[u8]>>(
+    hrp: S,
     data: T,
 ) -> Result<String, Bech32Error> {
     let data = super::base32::encode(data.as_ref())?;
-    bech32::encode(hrp, data, Variant::Bech32m).map_err(|e| e.into())
+    bech32::encode(hrp.as_ref(), data, Variant::Bech32m).map_err(|e| e.into())
 }
 
-pub fn bech32m_to_arbitrary_data(s: &str) -> Result<DecodedArbitraryDataFromBech32, Bech32Error> {
-    match bech32::decode(s) {
+pub fn bech32m_to_arbitrary_data<S: AsRef<str>>(
+    s: S,
+) -> Result<DecodedArbitraryDataFromBech32, Bech32Error> {
+    match bech32::decode(s.as_ref()) {
         Ok((hrp, base32, variant)) => {
             if variant == Variant::Bech32 {
                 return Err(Bech32Error::UnsupportedVariant);
