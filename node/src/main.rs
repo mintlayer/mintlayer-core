02a17876915e950b4ec0e7660411113fcda64978
Lukas Kuklinek <lukas.kuklinek@mintlayer.org>
2022-05-19 17:00:14 +0100

    node: Pass CLI arguments explicitly, better error presentation
    * `Options::from_args` now explicitly takes command line arguments
    * Make error printing more user friendly

--- node/src/main.rs
+++ node/src/main.rs
@@ -3,15 +3,19 @@
 mod runner;
 mod options;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let opts = options::Options::from_args();
+async fn run() -> anyhow::Result<()> {
+    let opts = options::Options::from_args(std::env::args_os());
 
     logging::init_logging(opts.log_path.as_ref());
     logging::log::trace!("Command line options: {:?}", opts);
 
-    let manager = runner::initialize(opts).await?;
+    runner::run(opts).await
+}
 
-    #[allow(clippy::unit_arg)]
-    Ok(manager.main().await)
+#[tokio::main]
+async fn main() {
+    run().await.unwrap_or_else(|err| {
+        eprintln!("ERROR: {}", err);
+        std::process::exit(1)
+    })
 }
