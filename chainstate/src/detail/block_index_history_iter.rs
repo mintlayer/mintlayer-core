3bf3bb89527feede9bc73afc74bf72822993342d
Samer Afach <info@afach.de>
2022-06-14 17:28:24 +0400

    Fix implementation and add test

--- chainstate/src/detail/block_index_history_iter.rs
+++ chainstate/src/detail/block_index_history_iter.rs
@@ -9,7 +9,7 @@ use super::consensus_validator::BlockIndexHandle;
 /// An iterator that starts at some block starting from a given it, and at every `next()` member call will provide the previous block index,
 /// The last viable block index is of the genesis block
 pub struct BlockIndexHistoryIterator<'a, H> {
-    next_id: Id<Block>,
+    next_id: Option<Id<Block>>,
     block_index_handle: &'a H,
 }
 
@@ -17,7 +17,7 @@ impl<'a, H: BlockIndexHandle> BlockIndexHistoryIterator<'a, H> {
     #[must_use]
     pub fn new(starting_id: Id<Block>, block_index_handle: &'a H) -> Self {
         Self {
-            next_id: starting_id,
+            next_id: Some(starting_id),
             block_index_handle,
         }
     }
@@ -27,22 +27,116 @@ impl<'a, H: BlockIndexHandle> Iterator for BlockIndexHistoryIterator<'a, H> {
     type Item = BlockIndex;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let result =
-            self.block_index_handle.get_block_index(&self.next_id).expect("Database error");
+        let result = match &self.next_id {
+            Some(next_id) => {
+                self.block_index_handle.get_block_index(&next_id).expect("Database error")
+            }
+            None => return None,
+        };
 
         let bi = match result {
             Some(bi) => bi,
             None => {
-                log::error!("CRITICAL: Invariant error; attempted to read id of a non-existent block index in iterator with id {}", self.next_id);
+                log::error!("CRITICAL: Invariant error; attempted to read id of a non-existent block index in iterator with id {:?}", self.next_id);
+                self.next_id = None;
                 return None;
             }
         };
 
-        match bi.get_prev_block_id() {
-            Some(next_id) => self.next_id = next_id.clone(),
-            None => (),
-        }
+        self.next_id = bi.get_prev_block_id().clone();
 
         Some(bi)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use blockchain_storage::Store;
+    use common::{
+        chain::{block::ConsensusData, config::create_unit_test_config},
+        primitives::{time, Idable, H256},
+    };
+
+    use crate::{BlockSource, Chainstate};
+
+    use super::*;
+
+    #[test]
+    fn mainnet_creation() {
+        common::concurrency::model(|| {
+            let chain_config = Arc::new(create_unit_test_config());
+            let storage = Store::new_empty().unwrap();
+            let mut chainstate = Chainstate::new(chain_config.clone(), storage, None).unwrap();
+
+            // put three blocks in a chain after genesis
+            let block1 = Block::new(
+                vec![],
+                Some(Id::new(&chainstate.chain_config.genesis_block_id().get())),
+                time::get() as u32,
+                ConsensusData::None,
+            )
+            .expect("Block creation failed");
+            chainstate.process_block(block1.clone(), BlockSource::Local).unwrap();
+
+            let block2 = Block::new(
+                vec![],
+                Some(block1.get_id()),
+                time::get() as u32,
+                ConsensusData::None,
+            )
+            .expect("Block creation failed");
+            chainstate.process_block(block2.clone(), BlockSource::Local).unwrap();
+
+            let block3 = Block::new(
+                vec![],
+                Some(block2.get_id()),
+                time::get() as u32,
+                ConsensusData::None,
+            )
+            .expect("Block creation failed");
+            chainstate.process_block(block3.clone(), BlockSource::Local).unwrap();
+
+            ///// test history iterator - start from tip
+            {
+                let chainstate_ref = chainstate.make_db_tx_ro();
+                let mut iter = BlockIndexHistoryIterator::new(block3.get_id(), &chainstate_ref);
+                assert_eq!(iter.next().unwrap().get_block_id(), &block3.get_id());
+                assert_eq!(iter.next().unwrap().get_block_id(), &block2.get_id());
+                assert_eq!(iter.next().unwrap().get_block_id(), &block1.get_id());
+                assert_eq!(
+                    iter.next().unwrap().get_block_id(),
+                    &chain_config.genesis_block_id()
+                );
+                assert!(iter.next().is_none());
+            }
+
+            ///// test history iterator - start from genesis
+            {
+                let chainstate_ref = chainstate.make_db_tx_ro();
+                let mut iter = BlockIndexHistoryIterator::new(
+                    chain_config.genesis_block_id(),
+                    &chainstate_ref,
+                );
+                assert_eq!(
+                    iter.next().unwrap().get_block_id(),
+                    &chain_config.genesis_block_id()
+                );
+                assert!(iter.next().is_none());
+            }
+
+            ///// test history iterator - start from an invalid non-existing block id
+            {
+                let chainstate_ref = chainstate.make_db_tx_ro();
+                let mut iter =
+                    BlockIndexHistoryIterator::new(Id::new(&H256::zero()), &chainstate_ref);
+
+                assert_ne!(iter.next_id, None); // ensure that we start with some id
+                assert!(iter.next().is_none());
+                assert_eq!(iter.next_id, None); // ensure that we won't be trying to read the db again
+                assert!(iter.next().is_none());
+            }
+        });
     }
 }
