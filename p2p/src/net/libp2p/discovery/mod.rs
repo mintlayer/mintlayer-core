6c7ea68f53c97bc81c0e4daaf0a1198cd6f08f37
Aaro Altonen <aaro.altonen@mintlayer.org>
2022-07-04 08:53:58 +0300

    net/libp2p: Introduce `DiscoveryManager`

---
+++ p2p/src/net/libp2p/discovery/mod.rs
@@ -0,0 +1,83 @@
+// Copyright (c) 2022 RBB S.r.l
+// opensource@mintlayer.org
+// SPDX-License-Identifier: MIT
+// Licensed under the MIT License;
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://spdx.org/licenses/MIT
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author(s): A. Altonen
+
+//! Discovery behaviour for libp2p
+
+use libp2p::{
+    core::{
+        PeerId,
+    },
+    mdns as libp2pmdns,
+    swarm::{
+        handler::DummyConnectionHandler,
+        NetworkBehaviourAction, PollParameters,
+    },
+    Multiaddr,
+};
+use std::{
+    task::{Context, Poll},
+};
+
+mod mdns;
+
+pub enum DiscoveryEvent {
+    /// Peer discovered
+    Discovered(Vec<(PeerId, Multiaddr)>),
+
+    /// Peer expired
+    Expired(Vec<(PeerId, Multiaddr)>),
+}
+
+pub struct DiscoveryManager {
+    /// Multicast DNS
+    mdns: mdns::Mdns,
+}
+
+impl DiscoveryManager {
+    pub fn new(mdns_enabled: bool) -> Self {
+        Self {
+            mdns: mdns::Mdns::new(mdns_enabled),
+        }
+    }
+
+    fn poll(
+        &mut self,
+        cx: &mut Context<'_>,
+        params: &mut impl PollParameters,
+    ) -> Poll<NetworkBehaviourAction<DiscoveryEvent, DummyConnectionHandler>> {
+        if let Poll::Ready(NetworkBehaviourAction::GenerateEvent(event)) =
+            self.mdns.poll(cx, params)
+        {
+            match event {
+                libp2pmdns::MdnsEvent::Discovered(addrs) => {
+                    return Poll::Ready(NetworkBehaviourAction::GenerateEvent(
+                        DiscoveryEvent::Discovered(addrs.collect::<Vec<_>>()),
+                    ));
+                }
+                libp2pmdns::MdnsEvent::Expired(addrs) => {
+                    return Poll::Ready(NetworkBehaviourAction::GenerateEvent(
+                        DiscoveryEvent::Expired(addrs.collect::<Vec<_>>()),
+                    ));
+                }
+            }
+        }
+
+        // TODO: poll future discovery strategies
+
+        Poll::Pending
+    }
+}
