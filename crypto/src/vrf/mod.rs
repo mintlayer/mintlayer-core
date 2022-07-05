a167cc414624e1fb04003dc0fdd518354a7c522f
Samer Afach <info@afach.de>
2022-07-05 15:46:21 +0400

    Tests for VRF interface

--- crypto/src/vrf/mod.rs
+++ crypto/src/vrf/mod.rs
@@ -6,9 +6,11 @@ mod schnorrkel;
 
 #[derive(Debug, PartialEq, Eq, Clone, Decode, Encode)]
 pub enum VRFKeyKind {
+    #[codec(index = 0)]
     Schnorrkel,
 }
 
+#[must_use]
 #[derive(Debug, PartialEq, Eq, Clone, Decode, Encode)]
 pub struct VRFPrivateKey {
     key: VRFPrivateKeyHolder,
@@ -16,9 +18,11 @@ pub struct VRFPrivateKey {
 
 #[derive(Debug, PartialEq, Eq, Clone, Decode, Encode)]
 pub(crate) enum VRFPrivateKeyHolder {
+    #[codec(index = 0)]
     Schnorrkel(schnorrkel::SchnorrkelPrivateKey),
 }
 
+#[must_use]
 #[derive(Debug, PartialOrd, Ord, PartialEq, Eq, Clone, Decode, Encode)]
 pub struct VRFPublicKey {
     pub_key: VRFPublicKeyHolder,
@@ -26,6 +30,7 @@ pub struct VRFPublicKey {
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Decode, Encode)]
 pub(crate) enum VRFPublicKeyHolder {
+    #[codec(index = 0)]
     Schnorrkel(schnorrkel::SchnorrkelPublicKey),
 }
 
@@ -67,5 +72,46 @@ impl VRFPublicKey {
                 ),
             },
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use hex::FromHex;
+    use serialization::DecodeAll;
+
+    use super::*;
+
+    #[test]
+    fn key_serialization() {
+        let (sk, pk) = VRFPrivateKey::new(VRFKeyKind::Schnorrkel);
+
+        let encoded_sk = sk.encode();
+        let encoded_pk = pk.encode();
+
+        let decoded_sk = VRFPrivateKey::decode_all(&mut encoded_sk.as_slice()).unwrap();
+        let decoded_pk = VRFPublicKey::decode_all(&mut encoded_pk.as_slice()).unwrap();
+
+        assert_eq!(sk, decoded_sk);
+        assert_eq!(pk, decoded_pk);
+
+        let encoded_sk_again = decoded_sk.encode();
+        let encoded_pk_again = decoded_pk.encode();
+
+        assert_eq!(encoded_sk, encoded_sk_again);
+        assert_eq!(encoded_pk, encoded_pk_again);
+    }
+
+    #[test]
+    fn fixed_keys() {
+        let encoded_sk :Vec<u8> = FromHex::from_hex("00c4546c0ad4d86dff34ff0459737e3bab90a0e3452c6deea6e8701a215ed45b0565d5ecaa1e36e81e5ad2b0fe884ed5d25f1de033d15cfb3ae8fb634ea130f93c").unwrap();
+        let encoded_pk: Vec<u8> =
+            FromHex::from_hex("006a9602eaae527451eed95667ecd4756324084e46b52fe908283c8b6b69095c09")
+                .unwrap();
+
+        let decoded_sk = VRFPrivateKey::decode_all(&mut encoded_sk.as_slice()).unwrap();
+        let decoded_pk = VRFPublicKey::decode_all(&mut encoded_pk.as_slice()).unwrap();
+
+        assert_eq!(decoded_pk, VRFPublicKey::from_private_key(&decoded_sk))
     }
 }
