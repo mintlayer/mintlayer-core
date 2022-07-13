7755c78f1c3caae0d144b7bd994d8e50b7fcc048
Samer Afach <info@afach.de>
2022-07-13 20:04:04 +0400

    Add delegation option

--- common/src/chain/transaction/output/stakelock.rs
+++ common/src/chain/transaction/output/stakelock.rs
@@ -6,6 +6,7 @@ use super::Destination;
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Encode, Decode)]
 pub struct LockedStakeData {
     owner: Destination,
+    staker: Option<Destination>,
     vrf_public_key: VRFPublicKey,
 }
 
@@ -16,5 +17,13 @@ impl LockedStakeData {
 
     pub fn vrf_public_key(&self) -> &VRFPublicKey {
         &self.vrf_public_key
+    }
+
+    pub fn staker(&self) -> &Destination {
+        self.staker.as_ref().unwrap_or(&self.owner)
+    }
+
+    pub fn is_delegated(&self) -> bool {
+        self.staker.is_some()
     }
 }
