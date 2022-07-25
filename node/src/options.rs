80d66772714b5d498c59606344ecaec86a0544a3
Stanislav Tkach <stanislav.tkach@gmail.com>
2022-07-25 11:30:54 +0200

    Remove coin decimals from options

--- node/src/options.rs
+++ node/src/options.rs
@@ -111,10 +111,6 @@ pub struct ChainConfigOptions {
     #[clap(long)]
     pub chain_target_block_spacing: Option<u64>,
 
-    /// Coin decimals.
-    #[clap(long)]
-    pub chain_coin_decimals: Option<u8>,
-
     /// The maximum block header size in bytes.
     #[clap(long)]
     pub chain_max_block_header_size: Option<usize>,
