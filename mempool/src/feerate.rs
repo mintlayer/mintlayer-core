51fc129ff6983450d6c7db6ed85eb3a87f4cd9f6
Roy Ben Abraham <roy.benabraham@rbblab.com>
2022-08-01 18:09:37 +0300

    Round up in fee computation

--- mempool/src/feerate.rs
+++ mempool/src/feerate.rs
@@ -27,7 +27,8 @@ impl FeeRate {
 
     pub(crate) fn compute_fee(&self, size: usize) -> Amount {
         let size = u128::try_from(size).expect("compute_fee conversion");
-        Amount::from_atoms((self.atoms_per_kb * size) / 1000)
+        // +999 for ceil operation
+        Amount::from_atoms((self.atoms_per_kb * size + 999) / 1000)
     }
 
     pub(crate) fn atoms_per_kb(&self) -> u128 {
