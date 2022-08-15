b465d900361c03fac82f97c7f61d73030c5d321e
Samer Afach <info@afach.de>
2022-08-15 20:15:55 +0400

    Add block reward getters + test getting it

--- chainstate/src/detail/chainstateref.rs
+++ chainstate/src/detail/chainstateref.rs
@@ -19,7 +19,9 @@ use chainstate_storage::{BlockchainStorageRead, BlockchainStorageWrite, Transact
 use chainstate_types::{get_skip_height, BlockIndex, GenBlockIndex, PropertyQueryError};
 use common::{
     chain::{
-        block::{calculate_tx_merkle_root, calculate_witness_merkle_root, BlockHeader},
+        block::{
+            calculate_tx_merkle_root, calculate_witness_merkle_root, BlockHeader, BlockReward,
+        },
         Block, ChainConfig, GenBlock, GenBlockId, OutPointSourceId,
     },
     primitives::{BlockDistance, BlockHeight, Id, Idable},
@@ -307,6 +309,13 @@ impl<'a, S: BlockchainStorageRead, O: OrphanBlocks> ChainstateRef<'a, S, O> {
             .chain_block_id()
             .ok_or(PropertyQueryError::GenesisHeaderRequested)?;
         Ok(self.get_block_index(&id)?.map(|block_index| block_index.into_block_header()))
+    }
+
+    pub fn get_block_reward(
+        &self,
+        block_index: &BlockIndex,
+    ) -> Result<Option<BlockReward>, PropertyQueryError> {
+        Ok(self.db_tx.get_block_reward(block_index)?)
     }
 
     pub fn get_block_height_in_main_chain(
