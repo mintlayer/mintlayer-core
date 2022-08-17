fe3aff4bfe1437200777aedea1114a85f60dcc1f
Samer Afach <info@afach.de>
2022-08-17 14:15:51 +0400

    Add license

--- chainstate-types/src/epoch_data.rs
+++ chainstate-types/src/epoch_data.rs
@@ -1,3 +1,18 @@
+// Copyright (c) 2022 RBB S.r.l
+// opensource@mintlayer.org
+// SPDX-License-Identifier: MIT
+// Licensed under the MIT License;
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://github.com/mintlayer/mintlayer-core/blob/master/LICENSE
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use common::primitives::H256;
 use serialization::{Decode, Encode};
 
