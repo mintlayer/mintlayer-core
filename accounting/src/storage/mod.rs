89cfebe32bec65bb4dfe1b42eb70c700dbcdbf0b
Samer Afach <info@afach.de>
2022-08-25 17:36:11 +0400

    Minor

--- accounting/src/storage/mod.rs
+++ accounting/src/storage/mod.rs
@@ -13,14 +13,14 @@ pub trait PoSAccountingStorageRead {
 
     fn get_pool_data(&self, pool_id: H256) -> Result<Option<PoolData>, Error>;
 
+    fn get_delegation_balance(&self, delegation_address: H256) -> Result<Option<Amount>, Error>;
+
+    fn get_delegation_data(&self, delegation_id: H256) -> Result<Option<DelegationData>, Error>;
+
     fn get_pool_delegations_shares(
         &self,
         pool_id: H256,
     ) -> Result<Option<BTreeMap<H256, Amount>>, Error>;
-
-    fn get_delegation_balance(&self, delegation_address: H256) -> Result<Option<Amount>, Error>;
-
-    fn get_delegation_data(&self, delegation_id: H256) -> Result<Option<DelegationData>, Error>;
 
     fn get_pool_delegation_share(
         &self,
