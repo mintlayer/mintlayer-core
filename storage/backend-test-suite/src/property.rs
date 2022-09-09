de651ec1a93058f30c3e110ddc5b1d4075c3b0cf
Lukas Kuklinek <lukas.kuklinek@mintlayer.org>
2022-09-09 09:08:41 +0100

    storage: Extend the backend test suite
    Mostly tests to do with the recently introduced key prefix iteration but
    includes some other new tests and test framework enhancements too.

--- storage/backend-test-suite/src/property.rs
+++ storage/backend-test-suite/src/property.rs
@@ -20,6 +20,7 @@ use proptest::prelude::Strategy;
 
 /// Proptest generators
 mod gen {
+    use super::WriteAction;
     pub use proptest::prelude::*;
     use storage_core::{Data, DbIndex};
 
@@ -33,13 +34,33 @@ mod gen {
     ) -> impl Strategy<Value = std::collections::BTreeMap<(DbIndex, Data), Data>> {
         proptest::collection::btree_map((idx(num_dbs), any::<Data>()), any::<Data>(), num_entries)
     }
+
+    // Generate key from a set of keys with given cardianlity. Lower cardinality encourages
+    // generation of conflicting keys, causing value overwrites and deletions to be more likely.
+    pub fn key(key_cardinality: u32) -> impl Strategy<Value = Data> {
+        (0..key_cardinality).prop_map(|x| format!("{x:x}").into())
+    }
+
+    pub fn action(key_cardinality: u32) -> impl Strategy<Value = WriteAction> {
+        prop_oneof![
+            (key(key_cardinality), any::<Data>()).prop_map(|(k, v)| WriteAction::Put(k, v)),
+            key(key_cardinality).prop_map(WriteAction::Del),
+        ]
+    }
+
+    pub fn actions(
+        key_cardinality: u32,
+        count: impl Into<proptest::collection::SizeRange>,
+    ) -> impl Strategy<Value = Vec<WriteAction>> {
+        proptest::collection::vec(action(key_cardinality), count)
+    }
 }
 
 fn overwrite_and_abort<B: Backend + ThreadSafe + Clone>(backend: B) {
     using_proptest(
         file!(),
         backend,
-        (gen::any::<Data>(), gen::any::<Data>(), gen::any::<Data>())
+        (gen::key(100), gen::any::<Data>(), gen::any::<Data>())
             .prop_filter("not equal", |(_, a, b)| a != b),
         |backend, (key, val0, val1)| {
             let store = backend.open(desc(1)).expect("db open to succeed");
@@ -101,7 +122,7 @@ fn add_and_delete<B: Backend + ThreadSafe + Clone>(backend: B) {
             // check all entries have been added
             let dbtx = store.transaction_ro();
             for ((db, key), val) in &entries {
-                assert_eq!(dbtx.get(*db, key).unwrap(), Some(val.as_ref()));
+                assert_eq!(dbtx.get(*db, key), Ok(Some(val.as_ref())));
             }
             drop(dbtx);
 
@@ -115,9 +136,34 @@ fn add_and_delete<B: Backend + ThreadSafe + Clone>(backend: B) {
             // Check entries no longer present
             let dbtx = store.transaction_ro();
             for (db, key) in entries.keys() {
-                assert_eq!(dbtx.get(*db, key).unwrap(), None);
+                assert_eq!(dbtx.get(*db, key), Ok(None));
             }
             drop(dbtx);
+        },
+    )
+}
+
+fn last_write_wins<B: Backend + ThreadSafe + Clone>(backend: B) {
+    using_proptest(
+        file!(),
+        backend,
+        (
+            gen::key(1000),
+            gen::prop::collection::vec(gen::any::<Data>(), 0..100),
+        ),
+        |backend, (key, vals)| {
+            let store = backend.open(desc(1)).expect("db open to succeed");
+            let last = vals.last().cloned();
+
+            // Add all entries to the database
+            let mut dbtx = store.transaction_rw();
+            for val in vals.into_iter() {
+                dbtx.put(IDX.0, key.clone(), val).unwrap();
+            }
+            dbtx.commit().unwrap();
+
+            let dbtx = store.transaction_ro();
+            assert_eq!(dbtx.get(IDX.0, key.as_ref()), Ok(last.as_deref()));
         },
     )
 }
@@ -146,7 +192,7 @@ fn add_and_delete_some<B: Backend + ThreadSafe + Clone>(backend: B) {
             let dbtx = store.transaction_ro();
             for ent @ (db, key) in entries1.keys().chain(entries2.keys()).chain(extra_keys.iter()) {
                 let expected = entries2.get(ent).or_else(|| entries1.get(ent)).map(AsRef::as_ref);
-                assert_eq!(dbtx.get(*db, key).unwrap(), expected);
+                assert_eq!(dbtx.get(*db, key), Ok(expected));
             }
             drop(dbtx);
 
@@ -161,15 +207,236 @@ fn add_and_delete_some<B: Backend + ThreadSafe + Clone>(backend: B) {
 
             // Check entries from the second set are absent
             for (db, key) in entries2.keys() {
-                assert_eq!(dbtx.get(*db, key).unwrap(), None);
+                assert_eq!(dbtx.get(*db, key), Ok(None));
             }
 
             // Check entries from the first set have correct value, unless deleted
             for ((db, key), val) in entries1.iter().filter(|e| !entries2.contains_key(e.0)) {
-                assert_eq!(dbtx.get(*db, key).unwrap(), Some(val.as_ref()));
+                assert_eq!(dbtx.get(*db, key), Ok(Some(val.as_ref())));
             }
         },
     )
 }
 
-tests![overwrite_and_abort, add_and_delete, add_and_delete_some];
+fn add_modify_abort_modify_commit<B: Backend + ThreadSafe + Clone>(backend: B) {
+    using_proptest(
+        file!(),
+        backend,
+        (
+            gen::actions(100, 0..20),
+            gen::actions(100, 0..20),
+            gen::actions(100, 0..20),
+        ),
+        |backend, (to_prepopulate, to_abort, to_commit)| {
+            let model = Model::from_actions(to_prepopulate.clone());
+            let store = backend.open(desc(1)).expect("db open to succeed");
+
+            // Pre-populate the db with initial data, check the contents agains the model
+            let mut dbtx = store.transaction_rw();
+            dbtx.apply_actions(IDX.0, to_prepopulate.into_iter());
+            dbtx.commit().unwrap();
+            assert_eq!(model, Model::from_db(&store, IDX.0));
+
+            // Apply another set of changes but abort the transaction
+            let tx_model = {
+                let mut tx_model = model.clone();
+                tx_model.extend(to_abort.clone().into_iter());
+                tx_model
+            };
+            let mut dbtx = store.transaction_rw();
+            dbtx.apply_actions(IDX.0, to_abort.into_iter());
+            assert_eq!(tx_model, Model::from_tx(&dbtx, IDX.0));
+            drop(dbtx);
+            assert_eq!(model, Model::from_db(&store, IDX.0));
+
+            // Apply a different set of operations, commit, check they have been performed
+            let model = {
+                let mut model = model;
+                model.extend(to_commit.clone().into_iter());
+                model
+            };
+            let mut dbtx = store.transaction_rw();
+            dbtx.apply_actions(IDX.0, to_commit.into_iter());
+            dbtx.commit().unwrap();
+            assert_eq!(model, Model::from_db(&store, IDX.0));
+        },
+    )
+}
+
+fn add_modify_abort_replay_commit<B: Backend + ThreadSafe + Clone>(backend: B) {
+    using_proptest(
+        file!(),
+        backend,
+        (gen::actions(100, 0..20), gen::actions(100, 0..20)),
+        |backend, (initial, actions)| {
+            let store = backend.open(desc(1)).expect("db open to succeed");
+
+            // Pre-populate the db with initial data, check the contents agains the model
+            let mut dbtx = store.transaction_rw();
+            dbtx.apply_actions(IDX.0, initial.into_iter());
+            dbtx.commit().unwrap();
+
+            let initial_model = Model::from_db(&store, IDX.0);
+
+            // Apply another set of changes but abort the transaction, check nothing changed
+            let mut dbtx = store.transaction_rw();
+            dbtx.apply_actions(IDX.0, actions.clone().into_iter());
+            let modified_model = Model::from_tx(&dbtx, IDX.0);
+            drop(dbtx);
+            assert_eq!(Model::from_db(&store, IDX.0), initial_model);
+
+            // Apply the same changes again, and check that we get to the same state after commit
+            let mut dbtx = store.transaction_rw();
+            dbtx.apply_actions(IDX.0, actions.into_iter());
+            dbtx.commit().unwrap();
+            assert_eq!(modified_model, Model::from_db(&store, IDX.0));
+        },
+    )
+}
+
+fn db_writes_do_not_interfere<B: Backend + ThreadSafe + Clone>(backend: B) {
+    using_proptest(
+        file!(),
+        backend,
+        (gen::actions(100, 0..20), gen::actions(100, 0..20)),
+        |backend, (actions0, actions1)| {
+            let store = backend.open(desc(2)).expect("db open to succeed");
+
+            // Apply one set of operations to key-value map 0
+            let mut dbtx = store.transaction_rw();
+            dbtx.apply_actions(IDX.0, actions0.into_iter());
+            dbtx.commit().unwrap();
+            let model = Model::from_db(&store, IDX.0);
+
+            // Apply another set of operations to key-value map 1
+            let mut dbtx = store.transaction_rw();
+            dbtx.apply_actions(IDX.1, actions1.into_iter());
+            dbtx.commit().unwrap();
+
+            // The values in key-value map 0 should remain untouched by the second set of changes
+            assert_eq!(model, Model::from_db(&store, IDX.0));
+        },
+    )
+}
+
+fn empty_after_abort<B: Backend + ThreadSafe + Clone>(backend: B) {
+    using_proptest(
+        file!(),
+        backend,
+        (
+            gen::actions(100, 0..20),
+            gen::prop::collection::vec(gen::key(100), 0..20),
+        ),
+        |backend, (actions, keys)| {
+            let store = backend.open(desc(5)).expect("db open to succeed");
+
+            // Apply one set of operations to key-value map 0
+            let model = Model::from_actions(actions.clone());
+            let mut dbtx = store.transaction_rw();
+            dbtx.apply_actions(IDX.0, actions.into_iter());
+            for key in &keys {
+                assert_eq!(dbtx.get(IDX.0, key), Ok(model.get(key)));
+            }
+            drop(dbtx);
+
+            let dbtx = store.transaction_ro();
+            for key in &keys {
+                assert_eq!(dbtx.get(IDX.0, key), Ok(None));
+            }
+        },
+    )
+}
+
+fn prefix_iteration<B: Backend + ThreadSafe + Clone>(backend: B) {
+    using_proptest(
+        file!(),
+        backend,
+        (gen::actions(100, 0..20), gen::actions(100, 0..20)),
+        |backend, (actions_a, actions_b)| {
+            // Add prefixes to action keys
+            fn add_prefix(pfx: u8, mut key: Data) -> Data {
+                key.insert(0, pfx);
+                key
+            }
+            let actions_a: Vec<WriteAction> =
+                actions_a.into_iter().map(|act| act.map_key(|k| add_prefix(b'a', k))).collect();
+            let actions_b: Vec<WriteAction> =
+                actions_b.into_iter().map(|act| act.map_key(|k| add_prefix(b'b', k))).collect();
+
+            // Open storage
+            let store = backend.open(desc(5)).expect("db open to succeed");
+
+            // Populate the database
+            let mut dbtx = store.transaction_rw();
+            dbtx.apply_actions(IDX.0, actions_a.iter().chain(actions_b.iter()).cloned());
+            dbtx.commit().unwrap();
+
+            // Check iteration over keys prefixed "a"
+            let model_a = Model::from_actions(actions_a);
+            let dbtx = store.transaction_ro();
+            let iter_a = dbtx.prefix_iter(IDX.0, vec![b'a']).unwrap();
+            assert!(model_a.into_iter().eq(iter_a));
+            drop(dbtx);
+
+            // Check iteration over keys prefixed "b"
+            let model_b = Model::from_actions(actions_b);
+            let dbtx = store.transaction_ro();
+            let iter_b = dbtx.prefix_iter(IDX.0, vec![b'b']).unwrap();
+            assert!(model_b.into_iter().eq(iter_b));
+            drop(dbtx);
+
+            // Check there are no entries prefixed "c"
+            let dbtx = store.transaction_ro();
+            assert_eq!(dbtx.prefix_iter(IDX.0, vec![b'c']).unwrap().next(), None);
+            drop(dbtx);
+
+            // Take all entries prefixed "a" and remove them
+            let mut dbtx = store.transaction_rw();
+            let keys_a: Vec<_> =
+                dbtx.prefix_iter(IDX.0, vec![b'a']).unwrap().map(|(k, _)| k).collect();
+            for key in keys_a {
+                dbtx.del(IDX.0, &key).unwrap();
+            }
+            dbtx.commit().unwrap();
+
+            // Check there are no entries prefixed "a"
+            let dbtx = store.transaction_ro();
+            assert_eq!(dbtx.prefix_iter(IDX.0, vec![b'a']).unwrap().next(), None);
+            drop(dbtx);
+        },
+    )
+}
+
+fn post_commit_consistency<B: Backend + ThreadSafe + Clone>(backend: B) {
+    using_proptest(
+        file!(),
+        backend,
+        gen::actions(100, 0..50),
+        |backend, actions| {
+            // Open storage
+            let store = backend.open(desc(1)).expect("db open to succeed");
+
+            let mut dbtx = store.transaction_rw();
+            dbtx.apply_actions(IDX.0, actions.into_iter());
+            let model = Model::from_tx(&dbtx, IDX.0);
+            dbtx.commit().unwrap();
+
+            // The state from the transaction just before committing should de the same as the
+            // state of the database after the commit.
+            assert_eq!(Model::from_db(&store, IDX.0), model);
+        },
+    )
+}
+
+tests![
+    add_and_delete,
+    add_and_delete_some,
+    add_modify_abort_modify_commit,
+    add_modify_abort_replay_commit,
+    db_writes_do_not_interfere,
+    empty_after_abort,
+    last_write_wins,
+    overwrite_and_abort,
+    post_commit_consistency,
+    prefix_iteration,
+];
