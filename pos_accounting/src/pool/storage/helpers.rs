b27185a5c7e184914fcd375b1bcd95f2010afb3c
Samer Afach <info@afach.de>
2022-09-19 12:39:47 +0400

    Isolate value wrappers based on trait bounds

--- pos_accounting/src/pool/storage/helpers.rs
+++ pos_accounting/src/pool/storage/helpers.rs
@@ -1,4 +1,7 @@
-use crate::{error::Error, storage::PoSAccountingStorageWrite};
+use crate::{
+    error::Error,
+    storage::{PoSAccountingStorageRead, PoSAccountingStorageWrite},
+};
 use chainstate_types::storage_result;
 
 pub struct StorageTempAccessor<'a, S, Getter, Setter, Deleter> {
@@ -8,9 +11,7 @@ pub struct StorageTempAccessor<'a, S, Getter, Setter, Deleter> {
     deleter: Deleter,
 }
 
-impl<'a, S: PoSAccountingStorageWrite, Getter, Setter, Deleter>
-    StorageTempAccessor<'a, S, Getter, Setter, Deleter>
-{
+impl<'a, S, Getter, Setter, Deleter> StorageTempAccessor<'a, S, Getter, Setter, Deleter> {
     pub fn new(store: &'a mut S, getter: Getter, setter: Setter, deleter: Deleter) -> Self {
         Self {
             store,
@@ -19,14 +20,22 @@ impl<'a, S: PoSAccountingStorageWrite, Getter, Setter, Deleter>
             deleter,
         }
     }
+}
 
+impl<'a, S: PoSAccountingStorageRead, Getter, Setter, Deleter>
+    StorageTempAccessor<'a, S, Getter, Setter, Deleter>
+{
     pub fn get<K: Ord + Copy, T: Clone>(&self, id: K) -> Result<Option<T>, Error>
     where
         Getter: Fn(&S, K) -> Result<Option<T>, storage_result::Error>,
     {
         (self.getter)(self.store, id).map_err(Error::StorageError)
     }
+}
 
+impl<'a, S: PoSAccountingStorageWrite, Getter, Setter, Deleter>
+    StorageTempAccessor<'a, S, Getter, Setter, Deleter>
+{
     pub fn set<K: Ord + Copy, T: Clone>(&mut self, id: K, value: T) -> Result<(), Error>
     where
         Setter: FnMut(&mut S, K, T) -> Result<(), storage_result::Error>,
