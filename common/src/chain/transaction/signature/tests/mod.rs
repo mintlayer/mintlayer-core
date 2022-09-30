98f93ccf2fb8a7cf1fea40d55bc2582c298a6020
Samer Afach <info@afach.de>
2022-09-30 10:44:11 +0400

    Minor

--- common/src/chain/transaction/signature/tests/mod.rs
+++ common/src/chain/transaction/signature/tests/mod.rs
@@ -275,8 +275,8 @@ fn check_insert_input(
     should_fail: bool,
 ) {
     let mut tx_updater = MutableTransaction::from(original_tx);
-    let outpoinr_source_id = OutPointSourceId::Transaction(Id::<Transaction>::new(H256::random()));
-    tx_updater.inputs.push(TxInput::new(outpoinr_source_id, 1));
+    let outpoint_source_id = OutPointSourceId::Transaction(Id::<Transaction>::new(H256::random()));
+    tx_updater.inputs.push(TxInput::new(outpoint_source_id, 1));
     tx_updater.witness.push(InputWitness::NoSignature(Some(vec![1, 2, 3])));
     let tx = tx_updater.generate_tx().unwrap();
     let res = verify_signature(destination, &tx, 0);
