404b55e7ace51d6e2a2e407177d2292b16b0a400
Samer Afach <info@afach.de>
2022-10-26 11:55:57 +0400

    Remove unused file

--- node/src/config_files/chainstate/mod.rs
+++ node/src/config_files/chainstate/mod.rs
@@ -13,8 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod storage_backend;
-
 use ::chainstate::ChainstateConfig;
 use serde::{Deserialize, Serialize};
 
