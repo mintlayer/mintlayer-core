81baa1f4471239e638ceeb343f5df3efa5cbf9fd
Lukas Kuklinek <lukas.kuklinek@mintlayer.org>
2023-01-06 18:32:25 +0000

    chainstate: Generalize `TransactionVerificationStrategy`

--- chainstate/test-framework/src/tx_verification_strategy/disposable_strategy.rs
+++ chainstate/test-framework/src/tx_verification_strategy/disposable_strategy.rs
@@ -55,21 +55,17 @@ impl TransactionVerificationStrategy for DisposableTransactionVerificationStrate
         &self,
         tx_verifier_maker: M,
         block_index_handle: &'a H,
-        storage_backend: &'a S,
+        storage_backend: S,
         chain_config: &'a ChainConfig,
         verifier_config: TransactionVerifierConfig,
         block_index: &'a BlockIndex,
         block: &WithId<Block>,
-    ) -> Result<TransactionVerifier<'a, &'a S, U>, BlockError>
+    ) -> Result<TransactionVerifier<'a, S, U>, BlockError>
     where
         H: BlockIndexHandle,
         S: TransactionVerifierStorageRef,
         U: UtxosView,
-        M: Fn(
-            &'a S,
-            &'a ChainConfig,
-            TransactionVerifierConfig,
-        ) -> TransactionVerifier<'a, &'a S, U>,
+        M: Fn(S, &'a ChainConfig, TransactionVerifierConfig) -> TransactionVerifier<'a, S, U>,
     {
         // The comparison for timelock is done with median_time_past based on BIP-113, i.e., the median time instead of the block timestamp
         let median_time_past =
@@ -134,19 +130,15 @@ impl TransactionVerificationStrategy for DisposableTransactionVerificationStrate
     fn disconnect_block<'a, S, M, U>(
         &self,
         tx_verifier_maker: M,
-        storage_backend: &'a S,
+        storage_backend: S,
         chain_config: &'a ChainConfig,
         verifier_config: TransactionVerifierConfig,
         block: &WithId<Block>,
-    ) -> Result<TransactionVerifier<'a, &'a S, U>, BlockError>
+    ) -> Result<TransactionVerifier<'a, S, U>, BlockError>
     where
         S: TransactionVerifierStorageRef,
         U: UtxosView,
-        M: Fn(
-            &'a S,
-            &'a ChainConfig,
-            TransactionVerifierConfig,
-        ) -> TransactionVerifier<'a, &'a S, U>,
+        M: Fn(S, &'a ChainConfig, TransactionVerifierConfig) -> TransactionVerifier<'a, S, U>,
     {
         let mut base_tx_verifier =
             tx_verifier_maker(storage_backend, chain_config, verifier_config);
