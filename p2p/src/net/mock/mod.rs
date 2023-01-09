9517feae0875a7d994e07b9081134cb2bafa5faf
Stanislav Tkach <stanislav.tkach@gmail.com>
2023-01-09 11:03:39 +0100

    Remove the PartialEq trait implementation from PeerInfo

--- p2p/src/net/mock/mod.rs
+++ p2p/src/net/mock/mod.rs
@@ -392,24 +392,22 @@ mod tests {
             conn1.poll_next().await
         {
             assert_eq!(address, conn2.local_addr().await.unwrap().unwrap());
+            assert_eq!(&peer_info.magic_bytes, config.magic_bytes());
+            assert_eq!(peer_info.version, SemVer::new(0, 1, 0));
+            assert_eq!(peer_info.agent, None);
             assert_eq!(
-                peer_info,
-                PeerInfo {
-                    peer_id: peer_info.peer_id,
-                    magic_bytes: *config.magic_bytes(),
-                    version: SemVer::new(0, 1, 0),
-                    agent: None,
-                    protocols: [
-                        Protocol::new(ProtocolType::PubSub, SemVer::new(1, 1, 0)),
-                        Protocol::new(ProtocolType::Ping, SemVer::new(1, 0, 0)),
-                        Protocol::new(ProtocolType::Sync, SemVer::new(0, 1, 0)),
-                    ]
-                    .into_iter()
-                    .collect(),
-                    subscriptions: [PubSubTopic::Blocks, PubSubTopic::Transactions]
-                        .into_iter()
-                        .collect(),
-                }
+                peer_info.protocols,
+                [
+                    Protocol::new(ProtocolType::PubSub, SemVer::new(1, 1, 0)),
+                    Protocol::new(ProtocolType::Ping, SemVer::new(1, 0, 0)),
+                    Protocol::new(ProtocolType::Sync, SemVer::new(0, 1, 0)),
+                ]
+                .into_iter()
+                .collect()
+            );
+            assert_eq!(
+                peer_info.subscriptions,
+                [PubSubTopic::Blocks, PubSubTopic::Transactions].into_iter().collect()
             );
         } else {
             panic!("invalid event received");
