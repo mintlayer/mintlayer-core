2921cb7797da108a2c067c97597ff9ee1e1c9d95
Alfie John <alfie@alfie.wtf>
2023-01-20 13:45:24 +1100

    Remove duplicated copyright, and update to this year

--- common/src/primitives/amount.rs
+++ common/src/primitives/amount.rs
@@ -1,26 +1,4 @@
-// Copyright (c) 2022 RBB S.r.l
-// opensource@mintlayer.org
-// SPDX-License-Identifier: MIT
-// Licensed under the MIT License;
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// https://github.com/mintlayer/mintlayer-core/blob/master/LICENSE
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#![allow(clippy::eq_op)]
-
-use serialization::{Decode, Encode};
-use std::iter::Sum;
-
-use super::signed_amount::SignedAmount;
-
-// Copyright (c) 2021 RBB S.r.l
+// Copyright (c) 2021-2023 RBB S.r.l
 // opensource@mintlayer.org
 // SPDX-License-Identifier: MIT
 // Licensed under the MIT License;
@@ -37,6 +15,14 @@ use super::signed_amount::SignedAmount;
 
 // use only unsigned types
 // if you need a signed amount, we should create a separate type for it and implement proper conversion
+
+#![allow(clippy::eq_op)]
+
+use serialization::{Decode, Encode};
+use std::iter::Sum;
+
+use super::signed_amount::SignedAmount;
+
 pub type UnsignedIntType = u128;
 
 /// An unsigned fixed-point type for amounts
