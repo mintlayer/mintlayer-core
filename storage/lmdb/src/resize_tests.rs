241bd3c30296a147833ef3f5ee807d31c5cde70b
Samer Afach <info@afach.de>
2023-02-02 20:08:48 +0400

    Appease loom

--- storage/lmdb/src/resize_tests.rs
+++ storage/lmdb/src/resize_tests.rs
@@ -55,153 +55,165 @@ fn create_random_data_map_with_target_byte_size(
 #[trace]
 #[case(test_utils::random::Seed::from_entropy())]
 fn auto_map_resize_between_txs(#[case] seed: Seed) {
-    let mut rng = make_seedable_rng(seed);
+    utils::concurrency::model(move || {
+        let mut rng = make_seedable_rng(seed);
 
-    let resize_actions = Arc::new(Mutex::new(Vec::new()));
+        let resize_actions = Arc::new(Mutex::new(Vec::new()));
 
-    let resize_actions_for_check = Arc::clone(&resize_actions);
-    let resize_actions = Arc::clone(&resize_actions);
-    let resize_callback = Box::new(move |v| resize_actions.lock().unwrap().push(v));
+        let resize_actions_for_check = Arc::clone(&resize_actions);
+        let resize_actions = Arc::clone(&resize_actions);
+        let resize_callback = Box::new(move |v| resize_actions.lock().unwrap().push(v));
 
-    let initial_map_size = 1 << 20;
+        let initial_map_size = 1 << 20;
 
-    let resize_settings = DatabaseResizeSettings {
-        min_resize_step: 1 << 16,
-        max_resize_step: 1 << 20,
-        default_resize_ratio_percentage: 10,
-        resize_trigger_percentage: 0.9,
-    };
+        let resize_settings = DatabaseResizeSettings {
+            min_resize_step: 1 << 16,
+            max_resize_step: 1 << 20,
+            default_resize_ratio_percentage: 10,
+            resize_trigger_percentage: 0.9,
+        };
 
-    let data_dir = TempDir::new("lmdb_resize").unwrap();
-    let lmdb = Lmdb::new(
-        data_dir.into_path(),
-        Some(initial_map_size),
-        resize_settings.clone(),
-        Some(resize_callback),
-    );
+        let data_dir = TempDir::new("lmdb_resize").unwrap();
+        let lmdb = Lmdb::new(
+            data_dir.into_path(),
+            Some(initial_map_size),
+            resize_settings.clone(),
+            Some(resize_callback),
+        );
 
-    let lmdb_impl = lmdb.open(DbDesc::from_iter(vec![MapDesc::new("SomeDb")])).unwrap();
+        let lmdb_impl = lmdb.open(DbDesc::from_iter(vec![MapDesc::new("SomeDb")])).unwrap();
 
-    // generate random values with a predefined target size that surpasses the current map size
-    let data =
-        create_random_data_map_with_target_byte_size(&mut rng, initial_map_size * 5, 500, 10000);
+        // generate random values with a predefined target size that surpasses the current map size
+        let data = create_random_data_map_with_target_byte_size(
+            &mut rng,
+            initial_map_size * 5,
+            500,
+            10000,
+        );
 
-    let mut resizes_via_commit_count = 0usize;
+        let mut resizes_via_commit_count = 0usize;
 
-    // write many key/val values, and while they're being written, expect that database map will grow
-    for (key, val) in &data {
-        let mut rw_tx = lmdb_impl.transaction_rw(None).unwrap();
-        rw_tx.put(DbIndex::new(0), key.clone(), val.clone()).unwrap();
-        match rw_tx.commit() {
-            Ok(_) => resizes_via_commit_count += 1,
-            Err(e) => panic!("Failed to commit: {e:?}"),
+        // write many key/val values, and while they're being written, expect that database map will grow
+        for (key, val) in &data {
+            let mut rw_tx = lmdb_impl.transaction_rw(None).unwrap();
+            rw_tx.put(DbIndex::new(0), key.clone(), val.clone()).unwrap();
+            match rw_tx.commit() {
+                Ok(_) => resizes_via_commit_count += 1,
+                Err(e) => panic!("Failed to commit: {e:?}"),
+            }
         }
-    }
 
-    let resize_action_result = resize_actions_for_check.lock().unwrap().clone();
-    assert!(!resize_action_result.is_empty());
-    for act in resize_action_result {
-        assert!(act.old_size < act.new_size);
-        assert!(act.new_size - act.old_size >= resize_settings.min_resize_step as u64);
-        assert!(act.new_size - act.old_size <= resize_settings.max_resize_step as u64);
-    }
+        let resize_action_result = resize_actions_for_check.lock().unwrap().clone();
+        assert!(!resize_action_result.is_empty());
+        for act in resize_action_result {
+            assert!(act.old_size < act.new_size);
+            assert!(act.new_size - act.old_size >= resize_settings.min_resize_step as u64);
+            assert!(act.new_size - act.old_size <= resize_settings.max_resize_step as u64);
+        }
 
-    // ensure data is successfully written
-    let ro_tx = lmdb_impl.transaction_ro().unwrap();
-    for (key, val) in data {
-        assert_eq!(ro_tx.get(DbIndex::new(0), &key).unwrap().unwrap(), val);
-    }
+        // ensure data is successfully written
+        let ro_tx = lmdb_impl.transaction_ro().unwrap();
+        for (key, val) in data {
+            assert_eq!(ro_tx.get(DbIndex::new(0), &key).unwrap().unwrap(), val);
+        }
 
-    assert!(resizes_via_commit_count > 0, "Not a single resize was scheduled after a transaction... this is very unlikely based on the test structure");
+        assert!(resizes_via_commit_count > 0, "Not a single resize was scheduled after a transaction... this is very unlikely based on the test structure");
+    })
 }
 
 #[rstest]
 #[trace]
 #[case(test_utils::random::Seed::from_entropy())]
 fn auto_map_resize_between_puts(#[case] seed: Seed) {
-    let mut rng = make_seedable_rng(seed);
+    utils::concurrency::model(move || {
+        let mut rng = make_seedable_rng(seed);
 
-    let resize_actions = Arc::new(Mutex::new(Vec::new()));
+        let resize_actions = Arc::new(Mutex::new(Vec::new()));
 
-    let resize_actions_for_check = Arc::clone(&resize_actions);
-    let resize_actions = Arc::clone(&resize_actions);
-    let resize_callback = Box::new(move |v| resize_actions.lock().unwrap().push(v));
+        let resize_actions_for_check = Arc::clone(&resize_actions);
+        let resize_actions = Arc::clone(&resize_actions);
+        let resize_callback = Box::new(move |v| resize_actions.lock().unwrap().push(v));
 
-    let initial_map_size = 1 << 20;
+        let initial_map_size = 1 << 20;
 
-    let resize_settings = DatabaseResizeSettings {
-        min_resize_step: 1 << 16,
-        max_resize_step: 1 << 20,
-        default_resize_ratio_percentage: 10,
-        resize_trigger_percentage: 0.9,
-    };
+        let resize_settings = DatabaseResizeSettings {
+            min_resize_step: 1 << 16,
+            max_resize_step: 1 << 20,
+            default_resize_ratio_percentage: 10,
+            resize_trigger_percentage: 0.9,
+        };
 
-    let data_dir = TempDir::new("lmdb_resize").unwrap();
-    let lmdb = Lmdb::new(
-        data_dir.into_path(),
-        Some(initial_map_size),
-        resize_settings.clone(),
-        Some(resize_callback),
-    );
+        let data_dir = TempDir::new("lmdb_resize").unwrap();
+        let lmdb = Lmdb::new(
+            data_dir.into_path(),
+            Some(initial_map_size),
+            resize_settings.clone(),
+            Some(resize_callback),
+        );
 
-    let lmdb_impl = lmdb.open(DbDesc::from_iter(vec![MapDesc::new("SomeDb")])).unwrap();
+        let lmdb_impl = lmdb.open(DbDesc::from_iter(vec![MapDesc::new("SomeDb")])).unwrap();
 
-    // generate random values with a predefined target size that surpasses the current map size
-    let data =
-        create_random_data_map_with_target_byte_size(&mut rng, initial_map_size * 5, 500, 10000);
+        // generate random values with a predefined target size that surpasses the current map size
+        let data = create_random_data_map_with_target_byte_size(
+            &mut rng,
+            initial_map_size * 5,
+            500,
+            10000,
+        );
 
-    // write many key/val values, and while they're being written, expect that database map will grow
-    // In this loop, we expect two things to schedule a resize:
-    // 1. While putting in the transaction
-    // 2. While committing a transaction
-    // We focus on while writing in the transaction
-    ////////////////////////////////////////////////
-    // Data stack is the full data that should be written to the database in a stack that we pop
-    let mut data_stack: Vec<(Vec<u8>, Vec<u8>)> = data.clone().into_iter().collect();
-    let mut resizes_via_put_count = 0usize;
-    'outer: loop {
-        let mut rw_tx = lmdb_impl.transaction_rw(None).unwrap();
-        let mut data_written_in_this_cycle = Vec::new();
-        while !data_stack.is_empty() {
-            let (key, val) = data_stack.last().unwrap();
-            match rw_tx.put(DbIndex::new(0), key.clone(), val.clone()) {
-                Ok(_) => {
-                    // on success, we continue writing and take record of that
-                    data_written_in_this_cycle.push(data_stack.pop().unwrap());
+        // write many key/val values, and while they're being written, expect that database map will grow
+        // In this loop, we expect two things to schedule a resize:
+        // 1. While putting in the transaction
+        // 2. While committing a transaction
+        // We focus on while writing in the transaction
+        ////////////////////////////////////////////////
+        // Data stack is the full data that should be written to the database in a stack that we pop
+        let mut data_stack: Vec<(Vec<u8>, Vec<u8>)> = data.clone().into_iter().collect();
+        let mut resizes_via_put_count = 0usize;
+        'outer: loop {
+            let mut rw_tx = lmdb_impl.transaction_rw(None).unwrap();
+            let mut data_written_in_this_cycle = Vec::new();
+            while !data_stack.is_empty() {
+                let (key, val) = data_stack.last().unwrap();
+                match rw_tx.put(DbIndex::new(0), key.clone(), val.clone()) {
+                    Ok(_) => {
+                        // on success, we continue writing and take record of that
+                        data_written_in_this_cycle.push(data_stack.pop().unwrap());
+                    }
+                    Err(_) => {
+                        // on failure, we expect a resize to happen after trying again, so we consider this cycle a failure and restore what we (unsuccessfully) wrote to the stack
+                        data_stack.extend(data_written_in_this_cycle);
+                        resizes_via_put_count += 1;
+                        continue 'outer;
+                    }
                 }
+            }
+            match rw_tx.commit() {
+                Ok(_) => break,
                 Err(_) => {
-                    // on failure, we expect a resize to happen after trying again, so we consider this cycle a failure and restore what we (unsuccessfully) wrote to the stack
+                    // on error, we continue, but return the data that failed writing to the stack
                     data_stack.extend(data_written_in_this_cycle);
-                    resizes_via_put_count += 1;
                     continue 'outer;
                 }
             }
         }
-        match rw_tx.commit() {
-            Ok(_) => break,
-            Err(_) => {
-                // on error, we continue, but return the data that failed writing to the stack
-                data_stack.extend(data_written_in_this_cycle);
-                continue 'outer;
-            }
+        let resize_action_result = resize_actions_for_check.lock().unwrap().clone();
+        assert!(!resize_action_result.is_empty());
+        for act in resize_action_result {
+            assert!(act.old_size < act.new_size);
+            assert!(act.new_size - act.old_size >= resize_settings.min_resize_step as u64);
+            assert!(act.new_size - act.old_size <= resize_settings.max_resize_step as u64);
         }
-    }
-    let resize_action_result = resize_actions_for_check.lock().unwrap().clone();
-    assert!(!resize_action_result.is_empty());
-    for act in resize_action_result {
-        assert!(act.old_size < act.new_size);
-        assert!(act.new_size - act.old_size >= resize_settings.min_resize_step as u64);
-        assert!(act.new_size - act.old_size <= resize_settings.max_resize_step as u64);
-    }
 
-    // ensure data is successfully written
-    let ro_tx = lmdb_impl.transaction_ro().unwrap();
-    for (key, val) in data {
-        assert_eq!(ro_tx.get(DbIndex::new(0), &key).unwrap().unwrap(), val);
-    }
+        // ensure data is successfully written
+        let ro_tx = lmdb_impl.transaction_ro().unwrap();
+        for (key, val) in data {
+            assert_eq!(ro_tx.get(DbIndex::new(0), &key).unwrap().unwrap(), val);
+        }
 
-    assert!(
+        assert!(
         resizes_via_put_count > 0,
         "Not a single resize was scheduled after a write/put... this is very unlikely based on the test structure"
     );
+    })
 }
