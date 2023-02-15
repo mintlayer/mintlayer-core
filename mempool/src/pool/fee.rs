c6f47debb70837f4d33bcaf157c598eb2560e30e
Samer Afach <info@afach.de>
2023-02-15 14:15:54 +0400

    Minor refactor

--- mempool/src/pool/fee.rs
+++ mempool/src/pool/fee.rs
@@ -19,19 +19,19 @@ impl Add for Fee {
     }
 }
 
+impl Sub for Fee {
+    type Output = Option<Self>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        (self.0 - rhs.0).map(Self)
+    }
+}
+
 impl Sum<Fee> for Option<Fee> {
     fn sum<I>(mut iter: I) -> Self
     where
         I: Iterator<Item = Fee>,
     {
         iter.try_fold(Fee(Amount::ZERO), std::ops::Add::add)
-    }
-}
-
-impl Sub for Fee {
-    type Output = Option<Self>;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        (self.0 - rhs.0).map(Self)
     }
 }
