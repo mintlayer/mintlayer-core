a57dd80d709a604c01dff5e2189b792b3142f589
Samer Afach <info@afach.de>
2023-02-24 18:33:45 +0400

    Use test cases to simplify single-proof testing

--- common/src/primitives/merkle/proof/single/tests.rs
+++ common/src/primitives/merkle/proof/single/tests.rs
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use rstest::rstest;
+
 use crate::primitives::{id::default_hash, merkle::tree::MerkleTree, H256};
 
 #[test]
@@ -22,43 +24,37 @@ fn single_proof_one_leaf() {
     let leaves = vec![v0];
     let t = MerkleTree::from_leaves(leaves.clone()).unwrap();
 
-    {
-        let leaf_index = 0;
-        let p0 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p0.branch().len(), 0);
+    let leaf_index = 0;
+    let p0 = t.proof_from_leaf(leaf_index).unwrap();
+    assert_eq!(p0.branch().len(), 0);
 
-        assert!(p0.into_values().verify(leaves[leaf_index], t.root()).is_none());
-    }
+    assert!(p0.into_values().verify(leaves[leaf_index], t.root()).is_none());
 }
 
-#[test]
-fn single_proof_two_leaves() {
+#[rstest]
+#[case(0, &[1])]
+#[case(1, &[0])]
+fn single_proof_two_leaves(#[case] leaf_index: usize, #[case] branch: &[usize]) {
     let v0 = default_hash(H256::zero());
     let v1 = default_hash(H256::from_low_u64_be(1));
 
     let leaves = vec![v0, v1];
     let t = MerkleTree::from_leaves(leaves.clone()).unwrap();
 
-    {
-        let leaf_index = 0;
-        let p0 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p0.branch().len(), 1);
-        assert_eq!(p0.branch()[0].abs_index(), 1);
-
-        assert!(p0.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
-    {
-        let leaf_index = 1;
-        let p1 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p1.branch().len(), 1);
-        assert_eq!(p1.branch()[0].abs_index(), 0);
-
-        assert!(p1.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
+    let p = t.proof_from_leaf(leaf_index).unwrap();
+    assert_eq!(
+        p.branch().iter().map(|n| n.abs_index()).collect::<Vec<_>>(),
+        branch
+    );
+    assert!(p.into_values().verify(leaves[leaf_index], t.root()).unwrap());
 }
 
-#[test]
-fn single_proof_four_leaves() {
+#[rstest]
+#[case(0, &[1,5])]
+#[case(1, &[0,5])]
+#[case(2, &[3,4])]
+#[case(3, &[2,4])]
+fn single_proof_four_leaves(#[case] leaf_index: usize, #[case] branch: &[usize]) {
     let v0 = default_hash(H256::zero());
     let v1 = default_hash(H256::from_low_u64_be(1));
     let v2 = default_hash(H256::from_low_u64_be(2));
@@ -67,46 +63,25 @@ fn single_proof_four_leaves() {
     let leaves = vec![v0, v1, v2, v3];
     let t = MerkleTree::from_leaves(leaves.clone()).unwrap();
 
-    {
-        let leaf_index = 0;
-        let p0 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p0.branch().len(), 2);
-        assert_eq!(p0.branch()[0].abs_index(), 1);
-        assert_eq!(p0.branch()[1].abs_index(), 5);
+    let p = t.proof_from_leaf(leaf_index).unwrap();
+    assert_eq!(
+        p.branch().iter().map(|n| n.abs_index()).collect::<Vec<_>>(),
+        branch
+    );
 
-        assert!(p0.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
-    {
-        let leaf_index = 1;
-        let p1 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p1.branch().len(), 2);
-        assert_eq!(p1.branch()[0].abs_index(), 0);
-        assert_eq!(p1.branch()[1].abs_index(), 5);
-
-        assert!(p1.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
-    {
-        let leaf_index = 2;
-        let p2 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p2.branch().len(), 2);
-        assert_eq!(p2.branch()[0].abs_index(), 3);
-        assert_eq!(p2.branch()[1].abs_index(), 4);
-
-        assert!(p2.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
-    {
-        let leaf_index = 3;
-        let p3 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p3.branch().len(), 2);
-        assert_eq!(p3.branch()[0].abs_index(), 2);
-        assert_eq!(p3.branch()[1].abs_index(), 4);
-
-        assert!(p3.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
+    assert!(p.into_values().verify(leaves[leaf_index], t.root()).unwrap());
 }
 
-#[test]
-fn single_proof_eight_leaves() {
+#[rstest]
+#[case(0, &[1,9,13])]
+#[case(1, &[0,9,13])]
+#[case(2, &[3,8,13])]
+#[case(3, &[2,8,13])]
+#[case(4, &[5,11,12])]
+#[case(5, &[4,11,12])]
+#[case(6, &[7,10,12])]
+#[case(7, &[6,10,12])]
+fn single_proof_eight_leaves(#[case] leaf_index: usize, #[case] branch: &[usize]) {
     let v0 = default_hash(H256::zero());
     let v1 = default_hash(H256::from_low_u64_be(1));
     let v2 = default_hash(H256::from_low_u64_be(2));
@@ -119,84 +94,11 @@ fn single_proof_eight_leaves() {
     let leaves = vec![v0, v1, v2, v3, v4, v5, v6, v7];
     let t = MerkleTree::from_leaves(leaves.clone()).unwrap();
 
-    {
-        let leaf_index = 0;
-        let p0 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p0.branch().len(), 3);
-        assert_eq!(p0.branch()[0].abs_index(), 1);
-        assert_eq!(p0.branch()[1].abs_index(), 9);
-        assert_eq!(p0.branch()[2].abs_index(), 13);
+    let p = t.proof_from_leaf(leaf_index).unwrap();
+    assert_eq!(
+        p.branch().iter().map(|n| n.abs_index()).collect::<Vec<_>>(),
+        branch
+    );
 
-        assert!(p0.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
-    {
-        let leaf_index = 1;
-        let p1 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p1.branch().len(), 3);
-        assert_eq!(p1.branch()[0].abs_index(), 0);
-        assert_eq!(p1.branch()[1].abs_index(), 9);
-        assert_eq!(p1.branch()[2].abs_index(), 13);
-
-        assert!(p1.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
-    {
-        let leaf_index = 2;
-        let p2 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p2.branch().len(), 3);
-        assert_eq!(p2.branch()[0].abs_index(), 3);
-        assert_eq!(p2.branch()[1].abs_index(), 8);
-        assert_eq!(p2.branch()[2].abs_index(), 13);
-
-        assert!(p2.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
-    {
-        let leaf_index = 3;
-        let p3 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p3.branch().len(), 3);
-        assert_eq!(p3.branch()[0].abs_index(), 2);
-        assert_eq!(p3.branch()[1].abs_index(), 8);
-        assert_eq!(p3.branch()[2].abs_index(), 13);
-
-        assert!(p3.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
-    {
-        let leaf_index = 4;
-        let p4 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p4.branch().len(), 3);
-        assert_eq!(p4.branch()[0].abs_index(), 5);
-        assert_eq!(p4.branch()[1].abs_index(), 11);
-        assert_eq!(p4.branch()[2].abs_index(), 12);
-
-        assert!(p4.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
-    {
-        let leaf_index = 5;
-        let p5 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p5.branch().len(), 3);
-        assert_eq!(p5.branch()[0].abs_index(), 4);
-        assert_eq!(p5.branch()[1].abs_index(), 11);
-        assert_eq!(p5.branch()[2].abs_index(), 12);
-
-        assert!(p5.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
-    {
-        let leaf_index = 6;
-        let p6 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p6.branch().len(), 3);
-        assert_eq!(p6.branch()[0].abs_index(), 7);
-        assert_eq!(p6.branch()[1].abs_index(), 10);
-        assert_eq!(p6.branch()[2].abs_index(), 12);
-
-        assert!(p6.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
-    {
-        let leaf_index = 7;
-        let p7 = t.proof_from_leaf(leaf_index).unwrap();
-        assert_eq!(p7.branch().len(), 3);
-        assert_eq!(p7.branch()[0].abs_index(), 6);
-        assert_eq!(p7.branch()[1].abs_index(), 10);
-        assert_eq!(p7.branch()[2].abs_index(), 12);
-
-        assert!(p7.into_values().verify(leaves[leaf_index], t.root()).unwrap());
-    }
+    assert!(p.into_values().verify(leaves[leaf_index], t.root()).unwrap());
 }
