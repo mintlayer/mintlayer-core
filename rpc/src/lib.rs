5691e155d19f1ce65d5af82733aafaeb9d75fee1
Pavel Kokolemin <pavelkok@gmail.com>
2023-04-21 08:47:46 +0300

    Update tower-http

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -30,6 +30,7 @@ pub use jsonrpsee::core::Error;
 pub use jsonrpsee::proc_macros::rpc;
 use rpc_auth::RpcAuth;
 use rpc_creds::RpcCreds;
+use tower_http::validate_request::ValidateRequestHeaderLayer;
 
 /// The Result type with RPC-specific error.
 pub type Result<T> = core::result::Result<T, Error>;
@@ -132,10 +133,7 @@ impl Rpc {
         creds: Option<RpcCreds>,
     ) -> anyhow::Result<Self> {
         let auth_layer = creds.as_ref().map(|creds| {
-            tower_http::auth::RequireAuthorizationLayer::custom(RpcAuth::new(
-                creds.username(),
-                creds.password(),
-            ))
+            ValidateRequestHeaderLayer::custom(RpcAuth::new(creds.username(), creds.password()))
         });
 
         let middleware = tower::ServiceBuilder::new().layer(tower::util::option_layer(auth_layer));
