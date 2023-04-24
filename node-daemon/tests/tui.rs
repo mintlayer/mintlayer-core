24608980f79bf4ef1d35bb898762c34da2405ab1
Samer Afach <info@afach.de>
2023-04-24 13:02:14 +0400

    Use name daemon instead of tui

--- node-daemon/tests/tui.rs
+++ node-daemon/tests/tui.rs
@@ -17,7 +17,7 @@ use std::path::Path;
 
 use assert_cmd::Command;
 
-const BIN_NAME: &str = env!("CARGO_BIN_EXE_node-tui");
+const BIN_NAME: &str = env!("CARGO_BIN_EXE_node-daemon");
 
 // This test is only needed because the node name ix hardcoded here, so if the name is changed we
 // get an error that is easy to understand.
