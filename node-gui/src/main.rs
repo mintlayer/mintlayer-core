97316242ee6fb62bc0e50bd841aa8ecd1fa85346
Samer Afach <info@afach.de>
2023-04-27 00:59:08 +0400

    Successful initialization of the node with issue on exit; subsystems don't shutdown properly

--- node-gui/src/main.rs
+++ node-gui/src/main.rs
@@ -1,57 +1,36 @@
-// Copyright (c) 2023 RBB S.r.l
-// opensource@mintlayer.org
-// SPDX-License-Identifier: MIT
-// Licensed under the MIT License;
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// https://github.com/mintlayer/mintlayer-core/blob/master/LICENSE
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+use std::fmt::Debug;
 use std::sync::Arc;
 
 use common::chain::ChainConfig;
-use iced::{
-    alignment::{Horizontal, Vertical},
-    widget::{Column, Container, Text},
-    Application, Element, Font, Length, Settings, Theme,
-};
-use iced_aw::{TabLabel, Tabs};
-
-mod dashboard;
-use dashboard::{DashboardMessage, DashboardTab};
-
-mod settings;
-use logging::log;
+use iced::widget::{column, container, text};
+use iced::{alignment, Subscription};
+use iced::{executor, Application, Command, Element, Length, Settings, Theme};
+use iced_aw::native::cupertino::cupertino_spinner::CupertinoSpinner;
 use node_lib::remote_controller::RemoteController;
-use settings::{SettingsMessage, SettingsTab, TabPosition};
+use subsystem::manager::ShutdownTrigger;
 use tokio::sync::oneshot;
 
-const HEADER_SIZE: u16 = 32;
-const TAB_PADDING: u16 = 16;
-
-const ICON_FONT: Font = iced::Font::External {
-    name: "Icons",
-    bytes: include_bytes!("../fonts/icons.ttf"),
-};
-
-enum Icon {
-    User,
-    CogAlt,
+pub fn main() -> iced::Result {
+    MintlayerSplash::run(Settings {
+        antialiasing: true,
+        exit_on_close_request: false,
+        try_opengles_first: true,
+        ..Settings::default()
+    })
 }
 
-impl From<Icon> for char {
-    fn from(icon: Icon) -> Self {
-        match icon {
-            Icon::User => '\u{E800}',
-            Icon::CogAlt => '\u{E802}',
-        }
-    }
+#[derive(Debug)]
+
+enum MintlayerSplash {
+    Loading,
+    Loaded(NodeController),
+}
+
+#[derive(Debug)]
+enum Message {
+    Loaded(anyhow::Result<NodeController>),
+    EventOccurred(iced_native::Event),
+    ShuttingDownFinished,
 }
 
 pub async fn initialize(
@@ -64,153 +43,158 @@ pub async fn initialize(
     node_lib::run(opts, Some(remote_controller_sender)).await
 }
 
-#[tokio::main]
-async fn main() -> iced::Result {
-    let (remote_controller_sender, remote_controller_receiver) = oneshot::channel();
-
-    let manager = initialize(remote_controller_sender).await.expect("Node initialization failed");
-    let shutdown_trigger = manager.make_shutdown_trigger();
-
-    let controller = remote_controller_receiver.await.expect("Node controller receiving failed");
-
-    let chain_config = controller
-        .chainstate
-        .call(|this| this.get_chain_config().clone())
-        .await
-        .expect("Chain config retrieval failed after node initialization");
-
-    let node_controller = NodeInitializationData {
-        chain_config,
-        controller,
-    };
-
-    let manager_join_handle = tokio::spawn(async move { manager.main().await });
-
-    let gui_settings = Settings {
-        antialiasing: true,
-        flags: Some(node_controller),
-        ..Settings::default()
-    };
-
-    tokio::task::block_in_place(|| MintlayerGUI::run(gui_settings))?;
-
-    shutdown_trigger.initiate();
-
-    {
-        let err = "Joining subsystem manager failed. Clean shutdown couldn't be performed.";
-        manager_join_handle.await.expect(err);
+fn do_shutdown(initialized_data: &mut NodeController) -> Command<Message> {
+    if initialized_data.manager_join_handle.is_none() {
+        return Command::none();
     }
+    logging::log::error!("Starting shutdown process");
+    let shutdown_trigger = initialized_data.shutdown_trigger.clone();
+    let mut join_handle = None;
+    std::mem::swap(&mut initialized_data.manager_join_handle, &mut join_handle);
 
-    Ok(())
+    Command::perform(
+        async move {
+            shutdown_trigger.initiate();
+            join_handle.expect("Must be found").await.expect("Joining failed");
+        },
+        |_| Message::ShuttingDownFinished,
+    )
 }
 
-struct MintlayerGUI {
-    active_tab: usize,
-    dashboard_tab: DashboardTab,
-    settings_tab: SettingsTab,
-}
-
-struct NodeInitializationData {
+struct NodeController {
     chain_config: Arc<ChainConfig>,
-    controller: RemoteController,
+    _controller: RemoteController,
+    shutdown_trigger: ShutdownTrigger,
+    manager_join_handle: Option<tokio::task::JoinHandle<()>>,
 }
 
-#[derive(Clone, Debug)]
-enum Message {
-    TabSelected(usize),
-    Dashboard(DashboardMessage),
-    Settings(SettingsMessage),
-}
-
-#[derive(Debug, Clone)]
-struct InitializationState {
-    hello: String,
-}
-
-impl InitializationState {
-    async fn load() -> Result<InitializationState, ()> {
-        log::info!("Starting node...");
-        tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
-        return Ok(Self {
-            hello: "Loaded!".to_string(),
-        });
+impl Debug for NodeController {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("NodeInitializationData")
+            .field("chain_config", &self.chain_config)
+            .finish()
     }
 }
 
-impl Application for MintlayerGUI {
-    type Message = Message;
-    type Executor = iced::executor::Default;
-    type Theme = Theme;
-    type Flags = Option<NodeInitializationData>;
+impl NodeController {
+    async fn load() -> anyhow::Result<NodeController> {
+        let (remote_controller_sender, remote_controller_receiver) = oneshot::channel();
 
-    fn new(init_data: Self::Flags) -> (Self, iced::Command<Self::Message>) {
-        let gui = MintlayerGUI {
-            active_tab: 0,
-            dashboard_tab: DashboardTab::new(),
-            settings_tab: SettingsTab::new(),
+        let manager =
+            initialize(remote_controller_sender).await.expect("Node initialization failed");
+        let shutdown_trigger = manager.make_shutdown_trigger();
+
+        let controller =
+            remote_controller_receiver.await.expect("Node controller receiving failed");
+
+        let manager_join_handle = tokio::spawn(async move { manager.main().await });
+
+        let chain_config = controller
+            .chainstate
+            .call(|this| this.get_chain_config().clone())
+            .await
+            .expect("Chain config retrieval failed after node initialization");
+
+        let node_controller = NodeController {
+            chain_config,
+            _controller: controller,
+            shutdown_trigger,
+            manager_join_handle: Some(manager_join_handle),
         };
 
+        Ok(node_controller)
+    }
+}
+
+impl Application for MintlayerSplash {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Theme = Theme;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Message>) {
         (
-            gui,
-            // TODO: put the initialization result here
-            iced::Command::perform(
-                InitializationState::load(),
-                |_: Result<InitializationState, ()>| Message::TabSelected(0),
-            ),
+            MintlayerSplash::Loading,
+            Command::perform(NodeController::load(), Message::Loaded),
         )
     }
 
     fn title(&self) -> String {
-        String::from("Mintlayer Node")
-    }
-
-    fn update(&mut self, message: Self::Message) -> iced::Command<Message> {
-        match message {
-            Message::TabSelected(selected) => self.active_tab = selected,
-            Message::Dashboard(message) => self.dashboard_tab.update(message),
-            Message::Settings(message) => self.settings_tab.update(message),
+        match self {
+            MintlayerSplash::Loading => ("Mintlayer Node - Loading...").to_string(),
+            MintlayerSplash::Loaded(d) => {
+                format!("Mintlayer Node - {}", d.chain_config.chain_type().name())
+            }
         }
-        iced::Command::none()
     }
 
-    fn view(&self) -> Element<'_, Self::Message> {
-        let position = self.settings_tab.settings().tab_bar_position.unwrap_or_default();
-        let theme = self.settings_tab.settings().tab_bar_theme.unwrap_or_default();
-
-        Tabs::new(self.active_tab, Message::TabSelected)
-            .push(self.dashboard_tab.tab_label(), self.dashboard_tab.view())
-            .push(self.settings_tab.tab_label(), self.settings_tab.view())
-            .tab_bar_style(theme)
-            .icon_font(ICON_FONT)
-            .tab_bar_position(match position {
-                TabPosition::Top => iced_aw::TabBarPosition::Top,
-                TabPosition::Bottom => iced_aw::TabBarPosition::Bottom,
-            })
-            .into()
-    }
-}
-
-trait Tab {
-    type Message;
-
-    fn title(&self) -> String;
-
-    fn tab_label(&self) -> TabLabel;
-
-    fn view(&self) -> Element<'_, Self::Message> {
-        let column = Column::new()
-            .spacing(20)
-            .push(Text::new(self.title()).size(HEADER_SIZE))
-            .push(self.content());
-
-        Container::new(column)
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .align_x(Horizontal::Center)
-            .align_y(Vertical::Center)
-            .padding(TAB_PADDING)
-            .into()
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match self {
+            MintlayerSplash::Loading => match message {
+                Message::Loaded(Ok(initialized_data)) => {
+                    *self = MintlayerSplash::Loaded(initialized_data);
+                    Command::none()
+                }
+                // While the screen is loading, ignore all events
+                Message::Loaded(Err(e)) => panic!("Error: {e}"),
+                Message::EventOccurred(event) => {
+                    if let iced::Event::Window(iced::window::Event::CloseRequested) = event {
+                        panic!("Attempted shutdown during initialization")
+                    } else {
+                        Command::none()
+                    }
+                }
+                Message::ShuttingDownFinished => Command::none(),
+            },
+            MintlayerSplash::Loaded(ref mut initialized_data) => {
+                match message {
+                    Message::Loaded(_) => Command::none(), // TODO: make this unreachable
+                    // TODO: handle error on initialization
+                    Message::EventOccurred(event) => {
+                        if let iced::Event::Window(iced::window::Event::CloseRequested) = event {
+                            do_shutdown(initialized_data)
+                        } else {
+                            Command::none()
+                        }
+                    }
+                    Message::ShuttingDownFinished => iced::window::close(),
+                }
+            }
+        }
     }
 
-    fn content(&self) -> Element<'_, Self::Message>;
+    fn view(&self) -> Element<Message> {
+        match self {
+            MintlayerSplash::Loading => {
+                container(CupertinoSpinner::new().width(Length::Fill).height(Length::Fill)).into()
+            }
+
+            MintlayerSplash::Loaded(state) => {
+                let main_widget = text(&format!(
+                    "Genesis block: {}",
+                    state.chain_config.genesis_block_id(),
+                ))
+                .width(Length::Fill)
+                .size(25)
+                .horizontal_alignment(alignment::Horizontal::Center)
+                .vertical_alignment(alignment::Vertical::Center);
+
+                let window_contents = column![main_widget];
+
+                container(window_contents)
+                    .width(Length::Fill)
+                    .height(Length::Fill)
+                    .center_y()
+                    .into()
+            }
+        }
+    }
+
+    fn theme(&self) -> Self::Theme {
+        Theme::Light
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        iced::subscription::events().map(Message::EventOccurred)
+    }
 }
