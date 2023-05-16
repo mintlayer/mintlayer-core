09bc64021dad018dc0d00dc00004b7534964980e
Pavel Kokolemin <pavelkok@gmail.com>
2023-05-16 07:35:22 +0300

    Move key chain tests to own files (wallet)

--- wallet/src/key_chain/account_key_chain/mod.rs
+++ wallet/src/key_chain/account_key_chain/mod.rs
@@ -306,44 +306,4 @@ impl AccountKeyChain {
 // TODO: tests
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::key_chain::MasterKeyChain;
-    use common::chain::config::create_unit_test_config;
-    use crypto::key::secp256k1::Secp256k1PublicKey;
-    use rstest::rstest;
-    use wallet_storage::{DefaultBackend, Store, TransactionRw, Transactional};
-    use wallet_types::account_info::DEFAULT_ACCOUNT_INDEX;
-
-    const MNEMONIC: &str = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
-
-    #[rstest]
-    #[case("03bf6f8d52dade77f95e9c6c9488fd8492a99c09ff23095caffb2e6409d1746ade")]
-    #[case("035df5d551bac1d61a5473615a70eb17b2f4ccbf7e354166639428941e4dbbcd81")]
-    #[case("030d1d07a8e45110d14f4e2c8623e8db556c11a90c0aac6be9a88f2464e446ee95")]
-    fn check_mine_methods(#[case] public: &str) {
-        let chain_config = Arc::new(create_unit_test_config());
-        let db = Arc::new(Store::new(DefaultBackend::new_in_memory()).unwrap());
-        let mut db_tx = db.transaction_rw(None).unwrap();
-
-        let master_key_chain =
-            MasterKeyChain::new_from_mnemonic(chain_config, &mut db_tx, MNEMONIC, None).unwrap();
-        let mut key_chain = master_key_chain
-            .create_account_key_chain(&mut db_tx, DEFAULT_ACCOUNT_INDEX)
-            .unwrap();
-        key_chain.top_up_all(&mut db_tx).unwrap();
-        db_tx.commit().unwrap();
-
-        // This public key should belong to the key chain
-        let pk: PublicKey =
-            Secp256k1PublicKey::from_bytes(&hex::decode(public).unwrap()).unwrap().into();
-        let pkh = PublicKeyHash::from(&pk);
-        let pk_destination = Destination::PublicKey(pk.clone());
-        let addr_destination = Destination::Address(pkh);
-
-        assert!(key_chain.is_public_key_mine(&pk));
-        assert!(key_chain.is_public_key_hash_mine(&pkh));
-        assert!(key_chain.is_destination_mine(&addr_destination));
-        assert!(key_chain.is_destination_mine(&pk_destination));
-    }
-}
+mod tests;
---
+++ wallet/src/key_chain/account_key_chain/tests.rs
@@ -0,0 +1,55 @@
+// Copyright (c) 2023 RBB S.r.l
+// opensource@mintlayer.org
+// SPDX-License-Identifier: MIT
+// Licensed under the MIT License;
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://github.com/mintlayer/mintlayer-core/blob/master/LICENSE
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use super::*;
+use crate::key_chain::MasterKeyChain;
+use common::chain::config::create_unit_test_config;
+use crypto::key::secp256k1::Secp256k1PublicKey;
+use rstest::rstest;
+use wallet_storage::{DefaultBackend, Store, TransactionRw, Transactional};
+use wallet_types::account_info::DEFAULT_ACCOUNT_INDEX;
+
+const MNEMONIC: &str =
+    "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
+
+#[rstest]
+#[case("03bf6f8d52dade77f95e9c6c9488fd8492a99c09ff23095caffb2e6409d1746ade")]
+#[case("035df5d551bac1d61a5473615a70eb17b2f4ccbf7e354166639428941e4dbbcd81")]
+#[case("030d1d07a8e45110d14f4e2c8623e8db556c11a90c0aac6be9a88f2464e446ee95")]
+fn check_mine_methods(#[case] public: &str) {
+    let chain_config = Arc::new(create_unit_test_config());
+    let db = Arc::new(Store::new(DefaultBackend::new_in_memory()).unwrap());
+    let mut db_tx = db.transaction_rw(None).unwrap();
+
+    let master_key_chain =
+        MasterKeyChain::new_from_mnemonic(chain_config, &mut db_tx, MNEMONIC, None).unwrap();
+    let mut key_chain = master_key_chain
+        .create_account_key_chain(&mut db_tx, DEFAULT_ACCOUNT_INDEX)
+        .unwrap();
+    key_chain.top_up_all(&mut db_tx).unwrap();
+    db_tx.commit().unwrap();
+
+    // This public key should belong to the key chain
+    let pk: PublicKey =
+        Secp256k1PublicKey::from_bytes(&hex::decode(public).unwrap()).unwrap().into();
+    let pkh = PublicKeyHash::from(&pk);
+    let pk_destination = Destination::PublicKey(pk.clone());
+    let addr_destination = Destination::Address(pkh);
+
+    assert!(key_chain.is_public_key_mine(&pk));
+    assert!(key_chain.is_public_key_hash_mine(&pkh));
+    assert!(key_chain.is_destination_mine(&addr_destination));
+    assert!(key_chain.is_destination_mine(&pk_destination));
+}
