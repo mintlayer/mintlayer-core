c98fd7e66239728f95a8b76d82d7460d53873737
Boris Oncev <boris.oncev@gmail.com>
2023-05-30 01:01:41 +0200

    Add MaybeEncrypted<T> to abstract the encryption
    - abstracted the optional encryption in the Storage
    - combined the locked bool and Optional<SymmKey> into a EncryptionState
      enum

--- wallet/storage/src/schema.rs
+++ wallet/storage/src/schema.rs
@@ -17,10 +17,11 @@
 
 use common::address::Address;
 use crypto::key::extended::ExtendedPublicKey;
+use utils::maybe_encrypted::MaybeEncrypted;
 use utxo::Utxo;
 use wallet_types::{
     AccountDerivationPathId, AccountId, AccountInfo, AccountKeyPurposeId, AccountOutPointId,
-    AccountTxId, KeychainUsageState, RootKeyId, WalletTx,
+    AccountTxId, KeychainUsageState, RootKeyContent, RootKeyId, WalletTx,
 };
 
 storage::decl_schema! {
@@ -33,7 +34,7 @@ storage::decl_schema! {
         /// Store keychain usage states
         pub DBKeychainUsageStates: Map<AccountKeyPurposeId, KeychainUsageState>,
         /// Store for all the private keys in this wallet
-        pub DBRootKeys: Map<RootKeyId, Vec<u8>>,
+        pub DBRootKeys: Map<RootKeyId, MaybeEncrypted<RootKeyContent>>,
         /// Store for all the public keys in this wallet
         pub DBPubKeys: Map<AccountDerivationPathId, ExtendedPublicKey>,
         /// Store for all the addresses that belong to an account
