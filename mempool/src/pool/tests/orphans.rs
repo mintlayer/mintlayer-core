a68df119c9c446ec189629ebf428887881375c07
Lukas Kuklinek <lukas.kuklinek@mintlayer.org>
2023-06-01 17:38:41 +0100

    mempool: Diamond graph test for orphans

--- mempool/src/pool/tests/orphans.rs
+++ mempool/src/pool/tests/orphans.rs
@@ -18,20 +18,22 @@ use common::primitives::id::hash_encoded;
 use super::*;
 
 #[rstest]
-#[case(
+#[trace]
+#[case::success(
     Seed::from_entropy(),
     Amount::from_atoms(100_000_000),
     Amount::from_atoms(90_000_000),
     true
 )]
-#[case(
+#[trace]
+#[case::failure(
     Seed::from_entropy(),
     Amount::from_atoms(90_000_000),
     Amount::from_atoms(100_000_000),
     false
 )]
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
-async fn simple_sequence(
+async fn two_transactions_in_sequence(
     #[case] seed: Seed,
     #[case] amt0: Amount,
     #[case] amt1: Amount,
@@ -82,10 +84,111 @@ async fn simple_sequence(
     assert_eq!(mempool.contains_transaction(&tx1_id), expected_in_mempool);
 }
 
+// Below, each test case encodes a sequence of transaction insertions.
+//
+// Each element of the Vec contains:
+// 0. Transaction to be inserted (see the diagram below)
+// 1. Expected state of the main (non-orphan) mempool. One bit per transaction, 1 = transaction is
+//    expected in mempool, 0 = transaction is expected not to be in mempool. Note that the lowest
+//    bit (rightmost digit) corresponds to transaction 0.
+// 2. Expected state of the orphan pool. Same encoding as above.
+//
+// The transaction topology used by the test is as follows:
+//
+// (i = input, o = output, [n] = transaction number as referred to in the code)
+//
+//                                   +-------+
+//                               +---i  [1]  o---+
+//                   +-------+   |   +-------+   |   +-------+
+// genesis UTXO -----i       o---+               +---i       o
+//                   |  [0]  |                       |  [3]  |
+//                   |       o---+               +---i       |
+//                   +-------+   |   +-------+   |   +-------+
+//                               +---i  [2]  o---+
+//                                   +-------+
+//
+// This is a simple diamond transaction graph but it covers many interesting cases.
+//
 #[rstest]
+#[trace]
+#[case::topological_order(
+    Seed::from_entropy(),
+    vec![(0, 0b0001, 0b0000), (1, 0b0011, 0b0000), (2, 0b0111, 0b0000), (3, 0b1111, 0b0000)],
+)]
+#[trace]
+#[case::op_branch_released_first(
+    Seed::from_entropy(),
+    vec![(1, 0b0000, 0b0010), (3, 0b0000, 0b1010), (0, 0b0011, 0b1000), (2, 0b1111, 0b0000)],
+)]
+#[trace]
+#[case::one_orphan_then_mempool(
+    Seed::from_entropy(),
+    vec![(1, 0b0000, 0b0010), (0, 0b0011, 0b0000), (2, 0b0111, 0b0000), (3, 0b1111, 0b0000)],
+)]
+#[trace]
+#[case::reverse_topological_order(
+    Seed::from_entropy(),
+    vec![(3, 0b0000, 0b1000), (2, 0b0000, 0b1100), (1, 0b0000, 0b1110), (0, 0b1111, 0b0000)],
+)]
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+async fn diamond_graph(#[case] seed: Seed, #[case] insertion_plan: Vec<(usize, usize, usize)>) {
+    let mut rng = make_seedable_rng(seed);
+    let tf = TestFramework::builder(&mut rng).build();
+    let genesis_id = tf.genesis().get_id();
+
+    // Set up the transactions
+
+    let mut make_tx = |ins: &[(OutPointSourceId, u32)], outs: &[u128]| {
+        let builder = ins.iter().fold(TransactionBuilder::new(), |b, (s, n)| {
+            b.add_input(TxInput::new(s.clone(), *n), empty_witness(&mut rng))
+        });
+        let builder = outs.iter().fold(builder, |b, a| {
+            b.add_output(TxOutput::Transfer(
+                OutputValue::Coin(Amount::from_atoms(*a)),
+                Destination::AnyoneCanSpend,
+            ))
+        });
+        builder.build()
+    };
+
+    let tx0 = make_tx(
+        &[(OutPointSourceId::BlockReward(genesis_id.into()), 0)],
+        &[100_000_000, 50_000_000],
+    );
+    let tx0_outpt = OutPointSourceId::Transaction(tx0.transaction().get_id());
+
+    let tx1 = make_tx(&[(tx0_outpt.clone(), 0)], &[80_000_000]);
+    let tx1_outpt = OutPointSourceId::Transaction(tx1.transaction().get_id());
+
+    let tx2 = make_tx(&[(tx0_outpt, 1)], &[30_000_000]);
+    let tx2_outpt = OutPointSourceId::Transaction(tx2.transaction().get_id());
+
+    let tx3 = make_tx(&[(tx1_outpt, 0), (tx2_outpt, 0)], &[90_000_000]);
+
+    let txs = vec![tx0, tx1, tx2, tx3];
+    let tx_ids: Vec<_> = txs.iter().map(|tx| tx.transaction().get_id()).collect();
+
+    // Set up mempool and execute the insertion plan
+    let mut mempool = setup_with_chainstate(tf.chainstate()).await;
+
+    for (tx_no, expected_mempool, expected_orphans) in insertion_plan {
+        let _ = mempool.add_transaction(txs[tx_no].clone()).expect("tx add");
+
+        // Check the expected mempool state
+        for (i, tx_id) in tx_ids.iter().enumerate() {
+            let expected_mempool = (expected_mempool >> i) & 1 != 0;
+            let expected_orphans = (expected_orphans >> i) & 1 != 0;
+            assert_eq!(mempool.contains_transaction(tx_id), expected_mempool);
+            assert_eq!(mempool.contains_orphan_transaction(tx_id), expected_orphans);
+        }
+    }
+}
+
+#[rstest]
+#[trace]
 #[case(Seed::from_entropy())]
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
-async fn sequence_permutation(#[case] seed: Seed) {
+async fn transaction_graph_subset_permutation(#[case] seed: Seed) {
     let mut rng = make_seedable_rng(seed);
 
     // Generate a valid graph of transactions
