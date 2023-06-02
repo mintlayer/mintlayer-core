747dcfb4fc93a4550b1d6b8ccd31394ba6a96dc3
Samer Afach <info@afach.de>
2023-06-02 12:02:27 +0400

    Fix do_checks to return error on failure of one of the tasks

--- do_checks.sh
+++ do_checks.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cargo fmt --check &&
-PYTHON=$(which python || which python3)
-$PYTHON .github/scripts/codecheck.py &&
+set -e
+cargo fmt --check
 cargo deny check --hide-inclusion-graph
-cargo clippy --all-features --workspace --all-targets -- -D warnings -A clippy::new_without_default -W clippy::implicit_saturating_sub -W clippy::implicit_clone -W clippy::map_unwrap_or -W clippy::unnested_or_patterns -W clippy::manual_assert -W clippy::unused_async -W clippy::mut_mut -W clippy::todo &&
+cargo clippy --all-features --workspace --all-targets -- -D warnings -A clippy::new_without_default -W clippy::implicit_saturating_sub -W clippy::implicit_clone -W clippy::map_unwrap_or -W clippy::unnested_or_patterns -W clippy::manual_assert -W clippy::unused_async -W clippy::mut_mut -W clippy::todo
 cargo clippy --all-features --workspace --lib --bins --examples -- -A clippy::all -D clippy::float_arithmetic -W clippy::unwrap_used -W clippy::dbg_macro -W clippy::items_after_statements -W clippy::fallible_impl_from -W clippy::string_slice
+PYTHON=$(which python || which python3)
+$PYTHON .github/scripts/codecheck.py
