9b84c37da3837ad5fce34733c248b596c5f82d79
Lukas Kuklinek <lukas.kuklinek@mintlayer.org>
2023-07-05 08:40:44 +0100

    p2p: Factor out `p2p::types` into a separate crate
    To correctly punish peers that submit invalid transactions that have
    been held up in orphan pool for a while, we need to track the origin of
    the transactions in question. That information will be propagated from
    p2p to mempool back to p2p. That means `mempool` needs the access to the
    `PeerId` type. To avoid circular dependencies, `p2p::types` is factored
    out into its own crate.

--- p2p/types/src/lib.rs
+++ p2p/types/src/lib.rs
@@ -13,6 +13,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod global_ip;
 pub mod ip_address;
 pub mod peer_address;
 pub mod peer_id;
+
+pub use global_ip::IsGlobalIp;
