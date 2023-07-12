20d54507c70f9f896bbf3273403f276755ad03bd
discosultan <jaanusvarus@gmail.com>
2023-07-12 06:16:13 +0200

    Add test when peer receives a header with invalid parent

--- p2p/src/sync/tests/ban_scores.rs
+++ p2p/src/sync/tests/ban_scores.rs
@@ -13,16 +13,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use chainstate::{ban_score::BanScore, BlockError, ChainstateError, CheckBlockError};
-use common::primitives::{Id, H256};
+use chainstate::{ban_score::BanScore, BlockError, BlockSource, ChainstateError, CheckBlockError};
+use chainstate_test_framework::{empty_witness, TestFramework, TransactionBuilder};
+use common::{
+    chain::{tokens::OutputValue, OutPointSourceId, TxInput, TxOutput},
+    primitives::{Amount, Id, Idable, H256},
+};
 use consensus::{ConsensusPoSError, ConsensusVerificationError};
+use crypto::random::Rng;
 use mempool::error::{MempoolPolicyError, TxValidationError};
+use test_utils::random::{make_seedable_rng, Seed};
 use tokio::sync::mpsc::unbounded_channel;
 
 use crate::{
     error::{P2pError, PeerError, ProtocolError},
+    message::{HeaderList, SyncMessage},
     net::default_backend::{transport::TcpTransportSocket, DefaultNetworkingService},
-    sync::peer::Peer,
+    sync::{peer::Peer, tests::helpers::SyncManagerHandle},
     types::peer_id::PeerId,
 };
 
@@ -75,4 +82,60 @@ async fn peer_handle_result() {
         .await;
         assert_eq!(handle_res, Ok(()));
     }
+}
+
+#[rstest::rstest]
+#[trace]
+#[case(Seed::from_entropy())]
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+async fn receive_header_with_invalid_parent_block(#[case] seed: Seed) {
+    let mut rng = make_seedable_rng(seed);
+    let mut tf = TestFramework::builder(&mut rng).build();
+
+    // Create and process an invalid block. It is invalid because an included transaction does not
+    // have any inputs and outputs. We deliberately ignore the processing error.
+    let invalid_tx = TransactionBuilder::new().build();
+    let invalid_parent_block = tf.make_block_builder().with_transactions(vec![invalid_tx]).build();
+    let invalid_parent_block_id = invalid_parent_block.get_id();
+    tf.process_block(invalid_parent_block, BlockSource::Local).unwrap_err();
+
+    // Create a valid block linked to the invalid parent block. Do not process it. Its header will
+    // be received from a peer instead.
+    let valid_tx = TransactionBuilder::new()
+        .add_input(
+            TxInput::from_utxo(
+                OutPointSourceId::BlockReward(tf.genesis().get_id().into()),
+                0,
+            ),
+            empty_witness(&mut rng),
+        )
+        .add_output(TxOutput::Burn(OutputValue::Coin(Amount::from_atoms(
+            rng.gen_range(100_000..200_000),
+        ))))
+        .build();
+    let valid_child_block = tf
+        .make_block_builder()
+        .with_transactions(vec![valid_tx])
+        .with_parent(invalid_parent_block_id.into())
+        .build();
+
+    // Connect a peer and have it send the child header. Ensure the peer's ban score is increased.
+    let mut handle =
+        SyncManagerHandle::builder().with_chainstate(tf.into_chainstate()).build().await;
+
+    let peer_id = PeerId::new();
+    handle.connect_peer(peer_id).await;
+
+    handle
+        .send_message(
+            peer_id,
+            SyncMessage::HeaderList(HeaderList::new(vec![valid_child_block.header().clone()])),
+        )
+        .await;
+
+    let (adjusted_peer_id, ban_score_delta) = handle.adjust_peer_score_event().await;
+    assert_eq!(adjusted_peer_id, peer_id);
+    assert_eq!(ban_score_delta, 100);
+
+    handle.join_subsystem_manager().await;
 }
