b4bc442ba650832b633e66e2ba10a573df5ed4a1
Alfie John <alfie@alfie.wtf>
2023-07-17 23:42:24 +1000

    Remove "mining" type from good script prefixes

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -508,7 +508,7 @@ class TestResult():
 def check_script_prefixes():
     """Check that test scripts start with one of the allowed name prefixes."""
 
-    good_prefixes_re = re.compile("^(blockprod|example|feature|interface|mempool|mining|p2p|rpc|wallet|tool)_")
+    good_prefixes_re = re.compile("^(blockprod|example|feature|interface|mempool|p2p|rpc|wallet|tool)_")
     bad_script_names = [script for script in ALL_SCRIPTS if good_prefixes_re.match(script) is None]
 
     if bad_script_names:
