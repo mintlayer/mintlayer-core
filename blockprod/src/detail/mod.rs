3665b4a383d284031199214da3d1f2f5aa7e4e24
Alfie John <alfie@alfie.wtf>
2023-07-24 14:52:29 +1000

    Don't produce blocks until Chainstate is up to date

--- blockprod/src/detail/mod.rs
+++ blockprod/src/detail/mod.rs
@@ -322,6 +322,16 @@ impl BlockProduction {
         transactions_source: TransactionsSource,
         custom_id_maybe: Option<Vec<u8>>,
     ) -> Result<(Block, oneshot::Receiver<usize>), BlockProductionError> {
+        let is_initial_block_download = self
+            .chainstate_handle
+            .call(|this| this.info().map(|info| info.is_initial_block_download))
+            .await?
+            .map_err(|_| BlockProductionError::ChainstateInfoRetrievalError)?;
+
+        if is_initial_block_download {
+          return Err(BlockProductionError::ChainstateWaitForSync);
+        }
+
         let current_peer_count = self
             .p2p_handle
             .call_async_mut(move |this| this.get_peer_count())
