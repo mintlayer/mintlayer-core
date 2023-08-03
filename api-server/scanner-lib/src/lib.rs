8730ed967bc82c846020fdc897ace1a3e78069b7
Pavel Kokolemin <pavelkok@gmail.com>
2023-08-03 10:17:11 +0300

    Add basic sync implementation (api server)

--- api-server/scanner-lib/src/lib.rs
+++ api-server/scanner-lib/src/lib.rs
@@ -3,4 +3,6 @@ use std::sync::Arc;
 use common::chain::ChainConfig;
 use node_comm::rpc_client::NodeRpcClient;
 
+pub mod sync;
+
 pub async fn run(_chain_config: &Arc<ChainConfig>, _rpc_client: &NodeRpcClient) {}
