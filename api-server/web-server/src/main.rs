1508a57a171c3a772f6a412414ee50a8669a0f7e
Samer Afach <info@afach.de>
2023-08-04 00:51:50 +0400

    License and appease clippy

--- api-server/web-server/src/main.rs
+++ api-server/web-server/src/main.rs
@@ -1,3 +1,18 @@
+// Copyright (c) 2023 RBB S.r.l
+// opensource@mintlayer.org
+// SPDX-License-Identifier: MIT
+// Licensed under the MIT License;
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://github.com/mintlayer/mintlayer-core/blob/master/LICENSE
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use axum::{
     http::StatusCode,
     routing::{get, post},
@@ -30,10 +45,12 @@ async fn main() {
 }
 
 // basic handler that responds with a static string
+#[allow(clippy::unused_async)]
 async fn root() -> &'static str {
     "Hello, World!"
 }
 
+#[allow(clippy::unused_async)]
 async fn create_user(
     // this argument tells axum to parse the request body
     // as JSON into a `CreateUser` type
