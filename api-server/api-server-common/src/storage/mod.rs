e0f57583916f542d36382f0182bf933e87018aec
Samer Afach <info@afach.de>
2023-08-06 04:44:59 +0400

    Add initialization functions to API server storage API

--- api-server/api-server-common/src/storage/mod.rs
+++ api-server/api-server-common/src/storage/mod.rs
@@ -13,5 +13,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod impls;
-mod storage_api;
+pub mod impls;
+pub mod storage_api;
--- api-server/api-server-common/src/storage/storage_api.rs
+++ api-server/api-server-common/src/storage/storage_api.rs
@@ -14,7 +14,7 @@
 // limitations under the License.
 
 use common::{
-    chain::{Block, Transaction},
+    chain::{Block, ChainConfig, GenBlock, Transaction},
     primitives::{BlockHeight, Id},
 };
 
@@ -25,12 +25,16 @@ pub enum ApiStorageError {
     StorageError(String),
     #[error("Deserialization error: {0}")]
     DeserializationError(String),
+    #[error("Storage initialization failed: {0}")]
+    InitializationError(String),
 }
 
 pub trait ApiStorageRead {
+    fn is_initialized(&self) -> Result<bool, ApiStorageError>;
+
     fn get_storage_version(&self) -> Result<Option<u32>, ApiStorageError>;
 
-    fn get_best_block(&self) -> Result<(BlockHeight, Id<Block>), ApiStorageError>;
+    fn get_best_block(&self) -> Result<(BlockHeight, Id<GenBlock>), ApiStorageError>;
 
     fn get_block(&self, block_id: Id<Block>) -> Result<Option<Block>, ApiStorageError>;
 
@@ -41,12 +45,14 @@ pub trait ApiStorageRead {
 }
 
 pub trait ApiStorageWrite: ApiStorageRead {
+    fn initialize_storage(&mut self, chain_config: &ChainConfig) -> Result<(), ApiStorageError>;
+
     fn set_storage_version(&mut self, version: u32) -> Result<(), ApiStorageError>;
 
     fn set_best_block(
         &mut self,
         block_height: BlockHeight,
-        block_id: Id<Block>,
+        block_id: Id<GenBlock>,
     ) -> Result<(), ApiStorageError>;
 
     fn set_block(&mut self, block_id: Id<Block>, block: Block) -> Result<(), ApiStorageError>;
