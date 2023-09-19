2f4ae01586f01a2b5d790ff5469fe756355b6774
Boris Oncev <boris.oncev@gmail.com>
2023-09-19 15:33:26 +0200

    Add info log when syncing

--- wallet/wallet-controller/src/sync/mod.rs
+++ wallet/wallet-controller/src/sync/mod.rs
@@ -145,6 +145,13 @@ pub async fn sync_once<T: NodeInterface>(
         )
         .await?;
 
+        let lowest_acc_height =
+            accounts_grouped.first().expect("empty accounts").0.common_block_height;
+        log::info!(
+            "Syncing the wallet from height: {} to {}",
+            lowest_acc_height,
+            chain_info.best_block_height
+        );
         // Sync all account groups together from last to first,
         // where the last has the lowest block height.
         // Once a group is synced with the next one, merge them,
