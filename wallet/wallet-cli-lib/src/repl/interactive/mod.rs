cd8b9ac16d0c36199add65d184d485acc4f7be6e
Samer Afach <info@afach.de>
2024-01-10 13:26:51 +0400

    Add a help message about auto-completion

--- wallet/wallet-cli-lib/src/repl/interactive/mod.rs
+++ wallet/wallet-cli-lib/src/repl/interactive/mod.rs
@@ -161,6 +161,7 @@ pub fn run(
 
     console.print_line("Use 'help' to see all available commands.");
     console.print_line("Use 'help <command>' to learn more about the parameters of the command.");
+    console.print_line("Press TAB on your keyboard to auto-complete any command you write.");
     console.print_line("Use 'exit' or Ctrl-D to quit.");
 
     loop {
