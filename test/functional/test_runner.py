5657aa876ef843691d736cc63154ce82f26ca8ad
Heorhii Azarov <heorhii.azarov@gmail.com>
2024-01-21 16:00:28 +0200

    Add functional test

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -113,6 +113,7 @@ BASE_SCRIPTS = [
     # vv Tests less than 2m vv
 
     # vv Tests less than 60s vv
+    'wallet_decommission_request.py',
 
     # vv Tests less than 30s vv
     'blockprod_generate_blocks_all_sources.py',
@@ -142,7 +143,6 @@ BASE_SCRIPTS = [
     'wallet_nfts.py',
     'wallet_delegations.py',
     'wallet_delegations_rpc.py',
-    'wallet_decommission_request.py',
     'wallet_high_fee.py',
     'wallet_generate_addresses.py',
     'wallet_set_lookahead_size.py',
