e140cfa55d48c92e0e8b6a487b3c7aa06cccd5a2
Lukas Kuklinek <lukas.kuklinek@mintlayer.org>
2024-03-04 18:25:15 +0000

    rpc: Unify docs generating tools in a separate crate

--- build-tools/code-docs/build-rpc-docs.sh
+++ build-tools/code-docs/build-rpc-docs.sh
@@ -5,5 +5,5 @@ set -x
 [ -f 'Cargo.toml' ] || exit 1
 grep 'name = "mintlayer-core"' Cargo.toml || exit 1
 
-cargo run -p node-lib --example node-rpc-docs >./node-daemon/RPC_INTERFACE.md
-cargo run -p wallet-rpc-lib --example wallet-rpc-docs >./wallet/wallet-rpc-daemon/RPC_INTERFACE.md
+cargo run -p gen-rpc-docs -- node >./node-daemon/RPC_INTERFACE.md
+cargo run -p gen-rpc-docs -- wallet >./wallet/wallet-rpc-daemon/RPC_INTERFACE.md
