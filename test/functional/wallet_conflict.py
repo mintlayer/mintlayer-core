4b5c27f26c803fdfbc98566cbda892067aad2faa
Boris Oncev <boris.oncev@gmail.com>
2024-03-04 17:44:43 +0100

    Add more asserts

--- test/functional/wallet_conflict.py
+++ test/functional/wallet_conflict.py
@@ -60,7 +60,8 @@ class WalletConflictTransaction(BitcoinTestFramework):
         block_input_data = block_input_data_obj.encode(block_input_data).to_hex()[2:]
 
         # create a new block, taking transactions from mempool
-        block = node.blockprod_generate_block(block_input_data, transactions, [], "FillSpaceFromMempool")
+        strategy = "FillSpaceFromMempool" if not transactions else "LeaveEmptySpace"
+        block = node.blockprod_generate_block(block_input_data, transactions, [], strategy)
         node.chainstate_submit_block(block)
         block_id = node.chainstate_best_block_id()
 
@@ -171,17 +172,24 @@ class WalletConflictTransaction(BitcoinTestFramework):
             assert_in("The transaction was marked as abandoned successfully", await wallet.abandon_transaction(transfer_tx_id))
 
             # create a block with the freeze token transaction
-            self.generate_block([freeze_tx])
+            block_id = self.generate_block([freeze_tx])
+            self.log.info(f"block with freeze tx {block_id}")
             assert_in("Success", await wallet.sync())
 
             # after the token is frozen the transfer token tx should be evicted by the mempool as conflicting
             # wait until mempool evicts the conflicting tx
             self.wait_until(lambda: not node.mempool_contains_tx(transfer_tx_id), timeout = 5)
+            assert not node.mempool_contains_orphan_tx(transfer_tx_id)
 
             assert_in("Success", await wallet.select_account(DEFAULT_ACCOUNT_INDEX))
             assert_in("The transaction was submitted successfully", await wallet.unfreeze_token(token_id))
-            self.generate_block()
+            block_id = self.generate_block()
+            self.log.info(f"block with un-freeze tx {block_id}")
             assert_in("Success", await wallet.sync())
+
+            # check it is not in the mempool or in orphans
+            assert not node.mempool_contains_tx(transfer_tx_id)
+            assert not node.mempool_contains_orphan_tx(transfer_tx_id)
 
             assert_in("Success", await wallet.select_account(1))
             assert_in("The transaction was submitted successfully", await wallet.send_tokens_to_address(token_id, address, 10))
