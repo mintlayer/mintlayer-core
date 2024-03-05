4fec0efaba7c46ca4dcaa66b475d7f2cfaa51b21
Samer Afach <info@afach.de>
2024-03-05 13:37:39 +0400

    Rename output files and put it in doc dirs

--- build-tools/code-docs/build-rpc-docs.sh
+++ build-tools/code-docs/build-rpc-docs.sh
@@ -5,5 +5,8 @@ set -x
 [ -f 'Cargo.toml' ] || exit 1
 grep 'name = "mintlayer-core"' Cargo.toml || exit 1
 
-cargo run -p gen-rpc-docs -- node >./node-daemon/RPC_INTERFACE.md
-cargo run -p gen-rpc-docs -- wallet >./wallet/wallet-rpc-daemon/RPC_INTERFACE.md
+mkdir -p ./node-daemon/docs/
+cargo run -p gen-rpc-docs -- node > ./node-daemon/docs/RPC.md
+
+mkdir -p ./wallet/wallet-rpc-daemon/docs/
+cargo run -p gen-rpc-docs -- wallet > ./wallet/wallet-rpc-daemon/docs/RPC.md
