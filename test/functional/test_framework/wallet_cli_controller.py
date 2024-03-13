d287fc6e01aa0a05cd80c7734d6b349afeaea986
Samer Afach <info@afach.de>
2024-03-13 15:29:39 +0400

    Fix pool information in wallet

--- test/functional/test_framework/wallet_cli_controller.py
+++ test/functional/test_framework/wallet_cli_controller.py
@@ -374,14 +374,14 @@ class WalletCliController:
     async def list_pool_ids(self) -> List[PoolData]:
         output = await self._write_command("staking-list-pools\n", can_be_empty=True)
         self.log.info(f"pools: {output}");
-        pattern = r"Pool Id: ([a-zA-Z0-9]+), Pledge: (\d+[.]?\d+), Balance: (\d+[.]?\d+), Creation Block Height: (\d+), Timestamp: (\d+), Staker: ([a-zA-Z0-9]+), Decommission Key: ([a-zA-Z0-9]+), VRF Public Key: ([a-zA-Z0-9]+)"
+        pattern = r"Pool Id: ([a-zA-Z0-9]+), Pledge: (\d+[.]?\d+), Balance: (\d+[.]?\d+), Creation Block Height: (\d+), Creation block timestamp: (\d+), Staker: ([a-zA-Z0-9]+), Decommission Key: ([a-zA-Z0-9]+), VRF Public Key: ([a-zA-Z0-9]+)"
         matches = re.findall(pattern, output)
         return [PoolData(pool_id, pledge, balance, int(height), timestamp, staker, decommission_key, vrf_public_key) for pool_id, pledge, balance, height, timestamp, staker, decommission_key, vrf_public_key in matches]
 
     async def list_pools_for_decommission(self) -> List[PoolData]:
         output = await self._write_command("staking-list-owned-pools-for-decommission\n", can_be_empty=True)
         self.log.info(f"pools: {output}");
-        pattern = r"Pool Id: ([a-zA-Z0-9]+), Pledge: (\d+[.]?\d+), Balance: (\d+[.]?\d+), Creation Block Height: (\d+), Timestamp: (\d+), Staker: ([a-zA-Z0-9]+), Decommission Key: ([a-zA-Z0-9]+), VRF Public Key: ([a-zA-Z0-9]+)"
+        pattern = r"Pool Id: ([a-zA-Z0-9]+), Pledge: (\d+[.]?\d+), Balance: (\d+[.]?\d+), Creation Block Height: (\d+), Creation block timestamp: (\d+), Staker: ([a-zA-Z0-9]+), Decommission Key: ([a-zA-Z0-9]+), VRF Public Key: ([a-zA-Z0-9]+)"
         matches = re.findall(pattern, output)
         return [PoolData(pool_id, pledge, balance, int(height), timestamp, staker, decommission_key, vrf_public_key) for pool_id, pledge, balance, height, timestamp, staker, decommission_key, vrf_public_key in matches]
 
