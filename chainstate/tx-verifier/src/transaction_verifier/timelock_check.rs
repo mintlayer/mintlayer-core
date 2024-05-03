f41a3432f9b326cc17381144536bd875703a5f00
Lukas Kuklinek <lukas.kuklinek@mintlayer.org>
2024-05-03 23:20:50 +0100

    tx-verifier: Introduce Context types
    These types hold information about the block and transaction being
    verified. The verification procedure interfaces with the context types
    rather than inspecting block / transaction directly.

--- chainstate/tx-verifier/src/transaction_verifier/timelock_check.rs
+++ chainstate/tx-verifier/src/transaction_verifier/timelock_check.rs
@@ -13,22 +13,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use chainstate_types::{block_index_ancestor_getter, GenBlockIndex};
 use common::{
-    chain::{
-        block::timestamp::BlockTimestamp, signature::Transactable, timelock::OutputTimeLock,
-        ChainConfig, GenBlock, TxInput, UtxoOutPoint,
-    },
-    primitives::{BlockCount, BlockDistance, BlockHeight, Id},
+    chain::{block::timestamp::BlockTimestamp, timelock::OutputTimeLock, UtxoOutPoint},
+    primitives::{BlockCount, BlockDistance, BlockHeight},
 };
 use thiserror::Error;
 use utils::ensure;
-use utxo::UtxosView;
 
-use super::{
-    error::ConnectTransactionError, storage::TransactionVerifierStorageRef,
-    TransactionSourceForConnect,
-};
+use super::error::ConnectTransactionError;
 
 #[derive(Error, Debug, PartialEq, Eq, Clone)]
 pub enum OutputMaturityError {
@@ -70,102 +62,6 @@ pub fn check_timelock(
         past_lock,
         ConnectTransactionError::TimeLockViolation(outpoint.clone())
     );
-
-    Ok(())
-}
-
-pub fn check_timelocks<S, C, T, U>(
-    storage: &S,
-    chain_config: &C,
-    utxos_view: &U,
-    tx: &T,
-    tx_source: &TransactionSourceForConnect,
-    spending_time: &BlockTimestamp,
-) -> Result<(), ConnectTransactionError>
-where
-    S: TransactionVerifierStorageRef,
-    C: AsRef<ChainConfig>,
-    T: Transactable,
-    U: UtxosView,
-{
-    let inputs = match tx.inputs() {
-        Some(inputs) => inputs,
-        None => return Ok(()),
-    };
-
-    let input_utxos = inputs
-        .iter()
-        .map(|input| match input {
-            TxInput::Utxo(outpoint) => {
-                let utxo = utxos_view.utxo(outpoint).map_err(|_| utxo::Error::ViewRead)?.ok_or(
-                    ConnectTransactionError::MissingOutputOrSpent(outpoint.clone()),
-                )?;
-                Ok(Some((outpoint.clone(), utxo)))
-            }
-            TxInput::Account(..) | TxInput::AccountCommand(..) => Ok(None),
-        })
-        .collect::<Result<Vec<_>, ConnectTransactionError>>()?;
-    debug_assert_eq!(inputs.len(), input_utxos.len());
-
-    let starting_point: GenBlockIndex = match tx_source {
-        TransactionSourceForConnect::Chain { new_block_index } => {
-            (*new_block_index).clone().into_gen_block_index()
-        }
-        TransactionSourceForConnect::Mempool {
-            current_best,
-            effective_height: _,
-        } => (*current_best).clone(),
-    };
-
-    // check if utxos can already be spent
-    for (outpoint, utxo) in input_utxos.iter().filter_map(|utxo| utxo.as_ref()) {
-        if let Some(timelock) = utxo.output().timelock() {
-            let (height, timestamp) = match utxo.source() {
-                utxo::UtxoSource::Blockchain(height) => {
-                    let block_index_getter = |db_tx: &S, _cc: &ChainConfig, id: &Id<GenBlock>| {
-                        db_tx.get_gen_block_index(id)
-                    };
-
-                    let source_block_index = block_index_ancestor_getter(
-                        block_index_getter,
-                        storage,
-                        chain_config.as_ref(),
-                        (&starting_point).into(),
-                        *height,
-                    )
-                    .map_err(|e| {
-                        ConnectTransactionError::InvariantErrorHeaderCouldNotBeLoadedFromHeight(
-                            e, *height,
-                        )
-                    })?;
-
-                    (*height, source_block_index.block_timestamp())
-                }
-                utxo::UtxoSource::Mempool => match tx_source {
-                    TransactionSourceForConnect::Chain { new_block_index: _ } => {
-                        unreachable!("Mempool utxos can never be reached from storage while connecting local transactions")
-                    }
-                    TransactionSourceForConnect::Mempool {
-                        current_best: _,
-                        effective_height,
-                    } => {
-                        // We're building upon another transaction in mempool. Treat it is as if it
-                        // was included at earliest possible time at earliest possible block.
-                        (*effective_height, *spending_time)
-                    }
-                },
-            };
-
-            check_timelock(
-                &height,
-                &timestamp,
-                timelock,
-                &tx_source.expected_block_height(),
-                spending_time,
-                outpoint,
-            )?;
-        }
-    }
 
     Ok(())
 }
