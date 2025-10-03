377af9d1802313bda0bd4261025c81c90a34491a
Mykhailo Kremniov <mkremniov@gmail.com>
2025-10-03 18:55:14 +0300

    Minor fix in build-tools/block-data-plots/collect_data.py

--- build-tools/block-data-plots/collect_data.py
+++ build-tools/block-data-plots/collect_data.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import subprocess
 from pathlib import Path
 
@@ -14,6 +15,7 @@ CHAIN_TYPE_CHOICES = ["mainnet", "testnet"]
 
 def collect_data(args):
     if args.output_file is None:
+        os.makedirs(DEFAULT_OUTPUT_DIR, exist_ok=True)
         output_file = DEFAULT_OUTPUT_DIR.joinpath(
             DEFAULT_OUTPUT_FILE_NAME_FMT.format(chain_type=args.chain_type)
         )
